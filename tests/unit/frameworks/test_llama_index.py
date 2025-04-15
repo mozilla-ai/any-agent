65ddc7cbb7a2e1c950292cbc1fe4385fc88c5679
daavoo <daviddelaiglesiacastro@gmail.com>
2025-04-15 13:58:09 +0200

    feat(llama_index): Support `managed_agents`.

--- tests/unit/frameworks/test_llama_index.py
+++ tests/unit/frameworks/test_llama_index.py
@@ -43,3 +43,55 @@ def test_load_llama_index_agent_missing():
     with patch("any_agent.frameworks.llama_index.llama_index_available", False):
         with pytest.raises(ImportError):
             AnyAgent.create(AgentFramework.LLAMAINDEX, AgentConfig(model_id="gpt-4o"))
+
+
+def test_load_langchain_multiagent():
+    model_mock = MagicMock()
+    create_mock = MagicMock()
+    agent_mock = MagicMock()
+    create_mock.return_value = agent_mock
+    tool_mock = MagicMock()
+    from llama_index.core.tools import FunctionTool
+
+    with (
+        patch("any_agent.frameworks.llama_index.ReActAgent", create_mock),
+        patch("any_agent.frameworks.llama_index.AgentWorkflow"),
+        patch("llama_index.llms.litellm.LiteLLM", model_mock),
+        patch.object(FunctionTool, "from_defaults", tool_mock),
+    ):
+        main_agent = AgentConfig(model_id="gpt-4.1-mini", description="Main agent")
+        managed_agents = [
+            AgentConfig(
+                model_id="gpt-4.1-nano",
+                tools=[
+                    "any_agent.tools.search_web",
+                    "any_agent.tools.visit_webpage",
+                ],
+                description="Managed agent",
+            ),
+        ]
+
+        AnyAgent.create(
+            AgentFramework.LLAMAINDEX, main_agent, managed_agents=managed_agents
+        )
+
+        create_mock.assert_any_call(
+            name="managed_agent_0",
+            description="Managed agent",
+            llm=model_mock.return_value,
+            tools=[
+                tool_mock(search_web),
+                tool_mock(visit_webpage),
+            ],
+            system_prompt=None,
+            can_handoff_to=["any_agent"],
+        )
+
+        create_mock.assert_called_with(
+            name="any_agent",
+            description="Main agent",
+            llm=model_mock.return_value,
+            system_prompt=None,
+            can_handoff_to=["managed_agent_0"],
+            tools=[],
+        )
