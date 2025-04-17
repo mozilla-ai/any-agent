flowportal-ai/belt

Only pass in a callable or MCPTool (#98)
* simplify tool loading

* work around smolagents bug

* fix unit test

* add verification for callables that they have some basic properties

* No more need to refresh tools

* rename file

src/any_agent/frameworks/google.py
@@ -4,7 +4,8 @@ from uuid import uuid4
 from flow_portal.config import AgentConfig, AgentFramework
 from flow_portal.frameworks.flow_portal import AnyAgent
 from flow_portal.logging import logger
-from flow_portal.tools.wrappers import import_and_wrap_tools
+from flow_portal.tools import search_web, visit_webpage
+from flow_portal.tools.wrappers import wrap_tools
 
 try:
     from google.adk.agents import Agent
@@ -41,10 +42,10 @@ class GoogleAgent(AnyAgent):
         """Load the Google agent with the given configuration."""
         if not self.managed_agents and not self.config.tools:
             self.config.tools = [
-                "flow_portal.tools.search_web",
-                "flow_portal.tools.visit_webpage",
+                search_web,
+                visit_webpage,
             ]
-        tools, mcp_servers = await import_and_wrap_tools(
+        tools, mcp_servers = await wrap_tools(
             self.config.tools, agent_framework=AgentFramework.GOOGLE
         )
         # Add to agent so that it doesn't get garbage collected
@@ -55,7 +56,7 @@ class GoogleAgent(AnyAgent):
         sub_agents_instanced = []
         if self.managed_agents:
             for managed_agent in self.managed_agents:
-                managed_tools, managed_mcp_servers = await import_and_wrap_tools(
+                managed_tools, managed_mcp_servers = await wrap_tools(
                     managed_agent.tools, agent_framework=AgentFramework.GOOGLE
                 )
                 # Add to agent so that it doesn't get garbage collected

src/any_agent/frameworks/llama_index.py
@@ -2,7 +2,8 @@ import importlib
 
 from flow_portal import AgentConfig, AgentFramework, AnyAgent
 from flow_portal.logging import logger
-from flow_portal.tools.wrappers import import_and_wrap_tools
+from flow_portal.tools import search_web, visit_webpage
+from flow_portal.tools.wrappers import wrap_tools
 
 try:
     from llama_index.core.agent.workflow import AgentWorkflow, ReActAgent
@@ -41,7 +42,7 @@ class LlamaIndexAgent(AnyAgent):
         return model_type(model=agent_config.model_id, **agent_config.model_args or {})
 
     async def _load_tools(self, tools):
-        imported_tools, mcp_servers = await import_and_wrap_tools(tools, self.framework)
+        imported_tools, mcp_servers = await wrap_tools(tools, self.framework)
 
         # Add to agent so that it doesn't get garbage collected
         self._mcp_servers.extend(mcp_servers)
@@ -56,8 +57,8 @@ class LlamaIndexAgent(AnyAgent):
 
         if not self.managed_agents and not self.config.tools:
             self.config.tools = [
-                "flow_portal.tools.search_web",
-                "flow_portal.tools.visit_webpage",
+                search_web,
+                visit_webpage,
             ]
 
         if self.managed_agents:

src/any_agent/frameworks/openai.py
@@ -4,7 +4,8 @@ from typing import Any
 from flow_portal.config import AgentConfig, AgentFramework
 from flow_portal.frameworks.flow_portal import AnyAgent
 from flow_portal.logging import logger
-from flow_portal.tools.wrappers import import_and_wrap_tools
+from flow_portal.tools import search_web, visit_webpage
+from flow_portal.tools.wrappers import wrap_tools
 
 try:
     from agents import (
@@ -61,17 +62,17 @@ class OpenAIAgent(AnyAgent):
 
         if not self.managed_agents and not self.config.tools:
             self.config.tools = [
-                "flow_portal.tools.search_web",
-                "flow_portal.tools.visit_webpage",
+                search_web,
+                visit_webpage,
             ]
-        tools, mcp_servers = await import_and_wrap_tools(
+        tools, mcp_servers = await wrap_tools(
             self.config.tools, agent_framework=AgentFramework.OPENAI
         )
 
         handoffs = []
         if self.managed_agents:
             for managed_agent in self.managed_agents:
-                managed_tools, managed_mcp_servers = await import_and_wrap_tools(
+                managed_tools, managed_mcp_servers = await wrap_tools(
                     managed_agent.tools, agent_framework=AgentFramework.OPENAI
                 )
                 kwargs = {}

tests/integration/test_load_and_run_multi_agent.py
@@ -3,6 +3,7 @@ import os
 import pytest
 
 from flow_portal import AgentConfig, AgentFramework, AnyAgent
+from flow_portal.tools import search_web, show_final_answer, visit_webpage
 from flow_portal.tracing import setup_tracing
 
 
@@ -13,7 +14,7 @@ from flow_portal.tracing import setup_tracing
     os.environ.get("ANY_AGENT_INTEGRATION_TESTS", "FALSE").upper() != "TRUE",
     reason="Integration tests require `ANY_AGENT_INTEGRATION_TESTS=TRUE` env var",
 )
-def test_load_and_run_multi_agent(framework, tmp_path, refresh_tools):
+def test_load_and_run_multi_agent(framework, tmp_path):
     agent_framework = AgentFramework(framework)
     kwargs = {}
     if framework == "smolagents":
@@ -32,18 +33,19 @@ def test_load_and_run_multi_agent(framework, tmp_path, refresh_tools):
         model_args={"parallel_tool_calls": False},
         **kwargs,
     )
+
     managed_agents = [
         AgentConfig(
             name="search_web_agent",
             model_id="gpt-4.1-nano",
             description="Agent that can search the web",
-            tools=["flow_portal.tools.search_web"],
+            tools=[search_web],
         ),
         AgentConfig(
             name="visit_webpage_agent",
             model_id="gpt-4.1-nano",
             description="Agent that can visit webpages",
-            tools=["flow_portal.tools.visit_webpage"],
+            tools=[visit_webpage],
         ),
     ]
     if framework != "smolagents":
@@ -52,7 +54,7 @@ def test_load_and_run_multi_agent(framework, tmp_path, refresh_tools):
                 name="final_answer_agent",
                 model_id="gpt-4.1-nano",
                 description="Agent that can show the final answer",
-                tools=["flow_portal.tools.show_final_answer"],
+                tools=[show_final_answer],
                 handoff=True,
             ),
         )

tests/integration/test_wrap_tools.py
@@ -8,13 +8,13 @@ from llama_index.core.tools import FunctionTool as LlamaindexClass
 from smolagents.tools import Tool as SmolagentsClass
 
 from flow_portal import AgentFramework
-from flow_portal.tools import visit_webpage
-from flow_portal.tools.wrappers import import_and_wrap_tools
+from flow_portal.tools import search_web, visit_webpage
+from flow_portal.tools.wrappers import wrap_tools
 
 
-def import_and_wrap_sync(tools, framework):
+def wrap_sync(tools, framework):
     wrapped_tools, _ = asyncio.get_event_loop().run_until_complete(
-        import_and_wrap_tools(tools, framework)
+        wrap_tools(tools, framework)
     )
     return wrapped_tools
 
@@ -29,8 +29,6 @@ def import_and_wrap_sync(tools, framework):
         ("smolagents", SmolagentsClass),
     ],
 )
-def test_import_and_wrap_tools(framework, expected_class):
-    wrapped_tools = import_and_wrap_sync(
-        ["flow_portal.tools.search_web", visit_webpage], AgentFramework(framework)
-    )
+def test_wrap_tools(framework, expected_class):
+    wrapped_tools = wrap_sync([search_web, visit_webpage], AgentFramework(framework))
     assert all(isinstance(tool, expected_class) for tool in wrapped_tools)

tests/unit/frameworks/test_openai.py
@@ -81,7 +81,7 @@ def test_load_openai_with_mcp_server():
     with (
         patch("flow_portal.frameworks.openai.Agent", mock_agent),
         patch("agents.function_tool", mock_function_tool),
-        patch("flow_portal.frameworks.openai.import_and_wrap_tools") as mock_wrap_tools,
+        patch("flow_portal.frameworks.openai.wrap_tools") as mock_wrap_tools,
     ):
         # Setup the mock to return tools and MCP servers
         mock_wrap_tools.return_value = (
@@ -121,24 +121,25 @@ def test_load_openai_multiagent():
         main_agent = AgentConfig(
             model_id="o3-mini",
         )
+
         managed_agents = [
             AgentConfig(
                 model_id="gpt-4o-mini",
                 name="user-verification-agent",
-                tools=["flow_portal.tools.ask_user_verification"],
+                tools=[ask_user_verification],
             ),
             AgentConfig(
                 model_id="gpt-4o",
                 name="search-web-agent",
                 tools=[
-                    "flow_portal.tools.search_web",
-                    "flow_portal.tools.visit_webpage",
+                    search_web,
+                    visit_webpage,
                 ],
             ),
             AgentConfig(
                 model_id="gpt-4o-mini",
                 name="communication-agent",
-                tools=["flow_portal.tools.show_final_answer"],
+                tools=[show_final_answer],
                 handoff=True,
             ),
         ]

mozilla-ai/any-agent

src/any_agent/frameworks/google.py
@@ -4,7 +4,8 @@ from uuid import uuid4
 from any_agent.config import AgentConfig, AgentFramework
 from any_agent.frameworks.any_agent import AnyAgent
 from any_agent.logging import logger
-from any_agent.tools.wrappers import import_and_wrap_tools
+from any_agent.tools import search_web, visit_webpage
+from any_agent.tools.wrappers import wrap_tools
 
 try:
     from google.adk.agents import Agent
@@ -41,10 +42,10 @@ class GoogleAgent(AnyAgent):
         """Load the Google agent with the given configuration."""
         if not self.managed_agents and not self.config.tools:
             self.config.tools = [
-                "any_agent.tools.search_web",
-                "any_agent.tools.visit_webpage",
+                search_web,
+                visit_webpage,
             ]
-        tools, mcp_servers = await import_and_wrap_tools(
+        tools, mcp_servers = await wrap_tools(
             self.config.tools, agent_framework=AgentFramework.GOOGLE
         )
         # Add to agent so that it doesn't get garbage collected
@@ -55,7 +56,7 @@ class GoogleAgent(AnyAgent):
         sub_agents_instanced = []
         if self.managed_agents:
             for managed_agent in self.managed_agents:
-                managed_tools, managed_mcp_servers = await import_and_wrap_tools(
+                managed_tools, managed_mcp_servers = await wrap_tools(
                     managed_agent.tools, agent_framework=AgentFramework.GOOGLE
                 )
                 # Add to agent so that it doesn't get garbage collected

src/any_agent/frameworks/llama_index.py
@@ -2,7 +2,8 @@ import importlib
 
 from any_agent import AgentConfig, AgentFramework, AnyAgent
 from any_agent.logging import logger
-from any_agent.tools.wrappers import import_and_wrap_tools
+from any_agent.tools import search_web, visit_webpage
+from any_agent.tools.wrappers import wrap_tools
 
 try:
     from llama_index.core.agent.workflow import AgentWorkflow, ReActAgent
@@ -41,7 +42,7 @@ class LlamaIndexAgent(AnyAgent):
         return model_type(model=agent_config.model_id, **agent_config.model_args or {})
 
     async def _load_tools(self, tools):
-        imported_tools, mcp_servers = await import_and_wrap_tools(tools, self.framework)
+        imported_tools, mcp_servers = await wrap_tools(tools, self.framework)
 
         # Add to agent so that it doesn't get garbage collected
         self._mcp_servers.extend(mcp_servers)
@@ -56,8 +57,8 @@ class LlamaIndexAgent(AnyAgent):
 
         if not self.managed_agents and not self.config.tools:
             self.config.tools = [
-                "any_agent.tools.search_web",
-                "any_agent.tools.visit_webpage",
+                search_web,
+                visit_webpage,
             ]
 
         if self.managed_agents:

src/any_agent/frameworks/openai.py
@@ -4,7 +4,8 @@ from typing import Any
 from any_agent.config import AgentConfig, AgentFramework
 from any_agent.frameworks.any_agent import AnyAgent
 from any_agent.logging import logger
-from any_agent.tools.wrappers import import_and_wrap_tools
+from any_agent.tools import search_web, visit_webpage
+from any_agent.tools.wrappers import wrap_tools
 
 try:
     from agents import (
@@ -61,17 +62,17 @@ class OpenAIAgent(AnyAgent):
 
         if not self.managed_agents and not self.config.tools:
             self.config.tools = [
-                "any_agent.tools.search_web",
-                "any_agent.tools.visit_webpage",
+                search_web,
+                visit_webpage,
             ]
-        tools, mcp_servers = await import_and_wrap_tools(
+        tools, mcp_servers = await wrap_tools(
             self.config.tools, agent_framework=AgentFramework.OPENAI
         )
 
         handoffs = []
         if self.managed_agents:
             for managed_agent in self.managed_agents:
-                managed_tools, managed_mcp_servers = await import_and_wrap_tools(
+                managed_tools, managed_mcp_servers = await wrap_tools(
                     managed_agent.tools, agent_framework=AgentFramework.OPENAI
                 )
                 kwargs = {}

tests/integration/test_load_and_run_multi_agent.py
@@ -3,6 +3,7 @@ import os
 import pytest
 
 from any_agent import AgentConfig, AgentFramework, AnyAgent
+from any_agent.tools import search_web, show_final_answer, visit_webpage
 from any_agent.tracing import setup_tracing
 
 
@@ -13,7 +14,7 @@ from any_agent.tracing import setup_tracing
     os.environ.get("ANY_AGENT_INTEGRATION_TESTS", "FALSE").upper() != "TRUE",
     reason="Integration tests require `ANY_AGENT_INTEGRATION_TESTS=TRUE` env var",
 )
-def test_load_and_run_multi_agent(framework, tmp_path, refresh_tools):
+def test_load_and_run_multi_agent(framework, tmp_path):
     agent_framework = AgentFramework(framework)
     kwargs = {}
     if framework == "smolagents":
@@ -32,18 +33,19 @@ def test_load_and_run_multi_agent(framework, tmp_path, refresh_tools):
         model_args={"parallel_tool_calls": False},
         **kwargs,
     )
+
     managed_agents = [
         AgentConfig(
             name="search_web_agent",
             model_id="gpt-4.1-nano",
             description="Agent that can search the web",
-            tools=["any_agent.tools.search_web"],
+            tools=[search_web],
         ),
         AgentConfig(
             name="visit_webpage_agent",
             model_id="gpt-4.1-nano",
             description="Agent that can visit webpages",
-            tools=["any_agent.tools.visit_webpage"],
+            tools=[visit_webpage],
         ),
     ]
     if framework != "smolagents":
@@ -52,7 +54,7 @@ def test_load_and_run_multi_agent(framework, tmp_path, refresh_tools):
                 name="final_answer_agent",
                 model_id="gpt-4.1-nano",
                 description="Agent that can show the final answer",
-                tools=["any_agent.tools.show_final_answer"],
+                tools=[show_final_answer],
                 handoff=True,
             ),
         )

tests/integration/test_wrap_tools.py
@@ -8,13 +8,13 @@ from llama_index.core.tools import FunctionTool as LlamaindexClass
 from smolagents.tools import Tool as SmolagentsClass
 
 from any_agent import AgentFramework
-from any_agent.tools import visit_webpage
-from any_agent.tools.wrappers import import_and_wrap_tools
+from any_agent.tools import search_web, visit_webpage
+from any_agent.tools.wrappers import wrap_tools
 
 
-def import_and_wrap_sync(tools, framework):
+def wrap_sync(tools, framework):
     wrapped_tools, _ = asyncio.get_event_loop().run_until_complete(
-        import_and_wrap_tools(tools, framework)
+        wrap_tools(tools, framework)
     )
     return wrapped_tools
 
@@ -29,8 +29,6 @@ def import_and_wrap_sync(tools, framework):
         ("smolagents", SmolagentsClass),
     ],
 )
-def test_import_and_wrap_tools(framework, expected_class):
-    wrapped_tools = import_and_wrap_sync(
-        ["any_agent.tools.search_web", visit_webpage], AgentFramework(framework)
-    )
+def test_wrap_tools(framework, expected_class):
+    wrapped_tools = wrap_sync([search_web, visit_webpage], AgentFramework(framework))
     assert all(isinstance(tool, expected_class) for tool in wrapped_tools)

tests/unit/frameworks/test_openai.py
@@ -81,7 +81,7 @@ def test_load_openai_with_mcp_server():
     with (
         patch("any_agent.frameworks.openai.Agent", mock_agent),
         patch("agents.function_tool", mock_function_tool),
-        patch("any_agent.frameworks.openai.import_and_wrap_tools") as mock_wrap_tools,
+        patch("any_agent.frameworks.openai.wrap_tools") as mock_wrap_tools,
     ):
         # Setup the mock to return tools and MCP servers
         mock_wrap_tools.return_value = (
@@ -121,24 +121,25 @@ def test_load_openai_multiagent():
         main_agent = AgentConfig(
             model_id="o3-mini",
         )
+
         managed_agents = [
             AgentConfig(
                 model_id="gpt-4o-mini",
                 name="user-verification-agent",
-                tools=["any_agent.tools.ask_user_verification"],
+                tools=[ask_user_verification],
             ),
             AgentConfig(
                 model_id="gpt-4o",
                 name="search-web-agent",
                 tools=[
-                    "any_agent.tools.search_web",
-                    "any_agent.tools.visit_webpage",
+                    search_web,
+                    visit_webpage,
                 ],
             ),
             AgentConfig(
                 model_id="gpt-4o-mini",
                 name="communication-agent",
-                tools=["any_agent.tools.show_final_answer"],
+                tools=[show_final_answer],
                 handoff=True,
             ),
         ]