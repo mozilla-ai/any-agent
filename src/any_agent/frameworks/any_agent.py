flowportal-ai/belt

Refactor MCP Servers to use discriminated unions (#115)
* refactor(mcp): divide each framework in different files and separate the logic for STDio and SSE

- Add AgnoMCPServer, GoogleMCPServer, LangchainMCPServer,
  LlamaIndexMCPServer, OpenAIMCPServer, and SmolagentsMCPServer
  classes to manage MCP connections for different frameworks.
- Update .pre-commit-config.yaml to include 'any-agent[dev,all]'.
- Refactor MCPServerBase to handle tool setup based on parameters.
- Enhance test coverage for MCP server functionalities.

* chore(pre-commit): update mypy dependencies

- Replace 'any-agent[dev,all]' with specific
  openinference instrumentation dependencies.
- Ensure compatibility with the latest
  openinference packages.

* refactor(mcp): reorder exports and clean up code

- Adjust the order of exports in __init__.py files for clarity.
- Update type hints to use string literals for better compatibility.
- Remove unnecessary blank lines to improve code readability.

* refactor(tests): update patches for MCP tests

* Apply pre-commit formatting

* refactor(mcp): improve MCP tool connection structure

- Refactor the MCP tool connection classes to enhance clarity and
  maintainability.
- Consolidate setup methods and streamline the connection logic.
- Update tests to reflect changes in the connection handling.

* refactor(mcp): enhance MCP tool connection handling

- Consolidate MCP tool connection logic by introducing a mapping
  from MCP parameters to connection types.
- Remove unused imports and improve code organization for clarity.

* refactor(mcp): simplify MCP tool connection handling

- Replace the MCP connection type mapping with a more concise
  annotated union type for AgnoMCPToolConnection.
- Update AgnoMCPServer to use the new connection type directly.
- Modify tests to validate the new initialization method for
  AgnoMCPServer.

* refactor(mcp): rename AgnoMCPServer to AgnoMCPToolConnection

- Update imports and references from AgnoMCPServer to
  AgnoMCP

src/any_agent/frameworks/any_agent.py
@@ -12,7 +12,7 @@ from flow_portal.tracing import setup_tracing
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
-    from flow_portal.tools.mcp import MCPServerBase
+    from flow_portal.tools.mcp.mcp_server import MCPServerBase
 
 
 class AnyAgent(ABC):

src/any_agent/tools/mcp/frameworks/agno.py
@@ -1,0 +1,73 @@
+import os
+from abc import ABC, abstractmethod
+from contextlib import suppress
+from typing import Literal
+
+from flow_portal.config import AgentFramework, MCPSseParams, MCPStdioParams
+from flow_portal.tools.mcp.mcp_server import MCPServerBase
+
+mcp_available = False
+with suppress(ImportError):
+    from agno.tools.mcp import MCPTools as AgnoMCPTools
+    from mcp import ClientSession
+    from mcp.client.sse import sse_client
+
+    mcp_available = True
+
+
+class AgnoMCPServerBase(MCPServerBase, ABC):
+    server: AgnoMCPTools | None = None
+    framework: Literal[AgentFramework.AGNO] = AgentFramework.AGNO
+
+    def check_dependencies(self) -> None:
+        """Check if the required dependencies for the MCP server are available."""
+        self.libraries = "any-agent[mcp,agno]"
+        self.mcp_available = mcp_available
+        super().check_dependencies()
+
+    @abstractmethod
+    async def setup_tools(self) -> None:
+        """Set up the Agno MCP server with the provided configuration."""
+        if not self.server:
+            msg = "MCP server is not set up. Please call `setup` from a concrete class."
+            raise ValueError(msg)
+
+        self.tools = [await self._exit_stack.enter_async_context(self.server)]
+
+
+class AgnoMCPServerStdio(AgnoMCPServerBase):
+    mcp_tool: MCPStdioParams
+
+    async def setup_tools(self) -> None:
+        server_params = f"{self.mcp_tool.command} {' '.join(self.mcp_tool.args)}"
+        self.server = AgnoMCPTools(
+            command=server_params,
+            include_tools=list(self.mcp_tool.tools or []),
+            env={**os.environ},
+        )
+
+        await super().setup_tools()
+
+
+class AgnoMCPServerSse(AgnoMCPServerBase):
+    mcp_tool: MCPSseParams
+
+    async def setup_tools(self) -> None:
+        client = sse_client(
+            url=self.mcp_tool.url,
+            headers=self.mcp_tool.headers,
+        )
+        sse_transport = await self._exit_stack.enter_async_context(client)
+        stdio, write = sse_transport
+        client_session = ClientSession(stdio, write)
+        session = await self._exit_stack.enter_async_context(client_session)
+        await session.initialize()
+        self.server = AgnoMCPTools(
+            session=session,
+            include_tools=self.mcp_tool.tools,
+        )
+
+        await super().setup_tools()
+
+
+AgnoMCPServer = AgnoMCPServerStdio | AgnoMCPServerSse

src/any_agent/tools/mcp/frameworks/google.py
@@ -1,0 +1,70 @@
+import os
+from abc import ABC, abstractmethod
+from contextlib import suppress
+from typing import Literal
+
+from flow_portal.config import AgentFramework, MCPSseParams, MCPStdioParams
+from flow_portal.tools.mcp.mcp_server import MCPServerBase
+
+mcp_available = False
+with suppress(ImportError):
+    from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset as GoogleMCPToolset
+    from google.adk.tools.mcp_tool.mcp_toolset import (
+        SseServerParams as GoogleSseServerParameters,
+    )
+    from google.adk.tools.mcp_tool.mcp_toolset import (
+        StdioServerParameters as GoogleStdioServerParameters,
+    )
+
+    mcp_available = True
+
+
+class GoogleMCPServerBase(MCPServerBase, ABC):
+    server: GoogleMCPToolset | None = None
+    framework: Literal[AgentFramework.GOOGLE] = AgentFramework.GOOGLE
+
+    def check_dependencies(self) -> None:
+        """Check if the required dependencies for the MCP server are available."""
+        self.libraries = "any-agent[mcp,google]"
+        self.mcp_available = mcp_available
+        super().check_dependencies()
+
+    @abstractmethod
+    async def setup_tools(self) -> None:
+        """Set up the Google MCP server with the provided configuration."""
+        if not self.server:
+            msg = "MCP server is not set up. Please call `setup` from a concrete class."
+            raise ValueError(msg)
+
+        await self._exit_stack.enter_async_context(self.server)
+        self.tools = await self.server.load_tools()
+
+
+class GoogleMCPServerStdio(GoogleMCPServerBase):
+    mcp_tool: MCPStdioParams
+
+    async def setup_tools(self) -> None:
+        params = GoogleStdioServerParameters(
+            command=self.mcp_tool.command,
+            args=list(self.mcp_tool.args),
+            env={**os.environ},
+        )
+        self.server = GoogleMCPToolset(connection_params=params)
+
+        await super().setup_tools()
+
+
+class GoogleMCPServerSse(GoogleMCPServerBase):
+    mcp_tool: MCPSseParams
+
+    async def setup_tools(self) -> None:
+        params = GoogleSseServerParameters(
+            url=self.mcp_tool.url,
+            headers=dict(self.mcp_tool.headers or {}),
+        )
+        self.server = GoogleMCPToolset(connection_params=params)
+
+        await super().setup_tools()
+
+
+GoogleMCPServer = GoogleMCPServerStdio | GoogleMCPServerSse

src/any_agent/tools/mcp/frameworks/llama_index.py
@@ -1,0 +1,64 @@
+import os
+from abc import ABC, abstractmethod
+from contextlib import suppress
+from typing import Literal
+
+from flow_portal.config import AgentFramework, MCPSseParams, MCPStdioParams
+from flow_portal.tools.mcp.mcp_server import MCPServerBase
+
+mcp_available = False
+with suppress(ImportError):
+    from llama_index.tools.mcp import BasicMCPClient as LlamaIndexMCPClient
+    from llama_index.tools.mcp import McpToolSpec as LlamaIndexMcpToolSpec
+
+    mcp_available = True
+
+
+class LlamaIndexMCPServerBase(MCPServerBase, ABC):
+    client: LlamaIndexMCPClient | None = None
+    framework: Literal[AgentFramework.LLAMA_INDEX] = AgentFramework.LLAMA_INDEX
+
+    def check_dependencies(self) -> None:
+        """Check if the required dependencies for the MCP server are available."""
+        self.libraries = "any-agent[mcp,llama_index]"
+        self.mcp_available = mcp_available
+        super().check_dependencies()
+
+    @abstractmethod
+    async def setup_tools(self) -> None:
+        """Set up the LlamaIndex MCP server with the provided configuration."""
+        if not self.client:
+            msg = "MCP client is not set up. Please call `setup` from a concrete class."
+            raise ValueError(msg)
+
+        mcp_tool_spec = LlamaIndexMcpToolSpec(
+            client=self.client,
+            allowed_tools=list(self.mcp_tool.tools or []),
+        )
+
+        self.tools = await mcp_tool_spec.to_tool_list_async()
+
+
+class LlamaIndexMCPServerStdio(LlamaIndexMCPServerBase):
+    mcp_tool: MCPStdioParams
+
+    async def setup_tools(self) -> None:
+        self.client = LlamaIndexMCPClient(
+            command_or_url=self.mcp_tool.command,
+            args=list(self.mcp_tool.args),
+            env={**os.environ},
+        )
+
+        await super().setup_tools()
+
+
+class LlamaIndexMCPServerSse(LlamaIndexMCPServerBase):
+    mcp_tool: MCPSseParams
+
+    async def setup_tools(self) -> None:
+        self.client = LlamaIndexMCPClient(command_or_url=self.mcp_tool.url)
+
+        await super().setup_tools()
+
+
+LlamaIndexMCPServer = LlamaIndexMCPServerStdio | LlamaIndexMCPServerSse

src/any_agent/tools/mcp/frameworks/smolagents.py
@@ -1,0 +1,93 @@
+import os
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+from contextlib import suppress
+from textwrap import dedent
+from typing import Literal
+
+from flow_portal.config import AgentFramework, MCPSseParams, MCPStdioParams
+from flow_portal.logging import logger
+from flow_portal.tools.mcp.mcp_server import MCPServerBase
+
+mcp_available = False
+with suppress(ImportError):
+    from mcp import StdioServerParameters
+    from smolagents.mcp_client import MCPClient
+    from smolagents.mcp_client import Tool as SmolagentsTool
+
+    mcp_available = True
+
+
+class SmolagentsMCPServerBase(MCPServerBase, ABC):
+    smolagent_tools: Sequence["SmolagentsTool"] | None = None
+    framework: Literal[AgentFramework.SMOLAGENTS] = AgentFramework.SMOLAGENTS
+
+    def check_dependencies(self) -> None:
+        """Check if the required dependencies for the MCP server are available."""
+        self.libraries = "any-agent[mcp,smolagents]"
+        self.mcp_available = mcp_available
+        super().check_dependencies()
+
+    @abstractmethod
+    async def setup_tools(self) -> None:
+        """Set up the Smolagents MCP server with the provided configuration."""
+        if not self.smolagent_tools:
+            msg = "Tool collection is not set up. Please call `setup` from a concrete class."
+            raise ValueError(msg)
+
+        # Only add the tools listed in mcp_tool['tools'] if specified
+        requested_tools = list(self.mcp_tool.tools or [])
+        if not requested_tools:
+            logger.info(
+                "No specific tools requested for MCP server, using all available tools:",
+            )
+            logger.info("Tools available: %s", self.smolagent_tools)
+            self.tools = self.smolagent_tools
+            return
+
+        filtered_tools = [
+            tool for tool in self.smolagent_tools if tool.name in requested_tools
+        ]
+        if len(filtered_tools) == len(requested_tools):
+            self.tools = filtered_tools
+            return
+
+        tool_names = [tool.name for tool in filtered_tools]
+        raise ValueError(
+            dedent(f"""Could not find all requested tools in the MCP server:
+                        Requested: {requested_tools}
+                        Set:   {tool_names}"""),
+        )
+
+
+class SmolagentsMCPServerStdio(SmolagentsMCPServerBase):
+    mcp_tool: MCPStdioParams
+
+    async def setup_tools(self) -> None:
+        server_parameters = StdioServerParameters(
+            command=self.mcp_tool.command,
+            args=list(self.mcp_tool.args),
+            env={**os.environ},
+        )
+        self.smolagent_tools = self._exit_stack.enter_context(
+            MCPClient(server_parameters)
+        )
+
+        await super().setup_tools()
+
+
+class SmolagentsMCPServerSse(SmolagentsMCPServerBase):
+    mcp_tool: MCPSseParams
+
+    async def setup_tools(self) -> None:
+        server_parameters = {
+            "url": self.mcp_tool.url,
+        }
+        self.smolagent_tools = self._exit_stack.enter_context(
+            MCPClient(server_parameters)
+        )
+
+        await super().setup_tools()
+
+
+SmolagentsMCPServer = SmolagentsMCPServerStdio | SmolagentsMCPServerSse

src/any_agent/tools/mcp/mcp_server.py
@@ -1,0 +1,34 @@
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+from contextlib import AsyncExitStack
+from typing import Any
+
+from pydantic import BaseModel, ConfigDict, Field, PrivateAttr
+
+from flow_portal.config import AgentFramework, MCPParams, Tool
+
+
+class MCPServerBase(BaseModel, ABC):
+    mcp_tool: MCPParams
+    framework: AgentFramework
+    mcp_available: bool = False
+    libraries: str = ""
+
+    _exit_stack: AsyncExitStack = PrivateAttr(default_factory=AsyncExitStack)
+    tools: Sequence[Tool] = Field(default_factory=list)
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    def model_post_init(self, context: Any) -> None:
+        self.check_dependencies()
+
+    @abstractmethod
+    async def setup_tools(self) -> None: ...
+
+    @abstractmethod
+    def check_dependencies(self) -> None:
+        if self.mcp_available:
+            return
+
+        msg = f"You need to `pip install '{self.libraries}'` to use MCP."
+        raise ImportError(msg)

src/any_agent/tools/wrappers.py
@@ -4,14 +4,9 @@ from functools import wraps
 from typing import Any
 
 from flow_portal.config import AgentFramework, MCPParams, Tool
-from flow_portal.tools.mcp import (
-    AgnoMCPServer,
-    GoogleMCPServer,
-    LangchainMCPServer,
-    LlamaIndexMCPServer,
+from flow_portal.tools import (
     MCPServerBase,
-    OpenAIMCPServer,
-    SmolagentsMCPServer,
+    get_mcp_server,
 )
 
 
@@ -75,22 +70,7 @@ async def wrap_mcp_server(
     """Generic MCP server wrapper that can work with different frameworks
     based on the specified agent_framework
     """
-    # Select the appropriate manager based on agent_framework
-    mcp_server_map: dict[AgentFramework, type[MCPServerBase]] = {
-        AgentFramework.OPENAI: OpenAIMCPServer,
-        AgentFramework.SMOLAGENTS: SmolagentsMCPServer,
-        AgentFramework.LANGCHAIN: LangchainMCPServer,
-        AgentFramework.GOOGLE: GoogleMCPServer,
-        AgentFramework.LLAMA_INDEX: LlamaIndexMCPServer,
-        AgentFramework.AGNO: AgnoMCPServer,
-    }
-    if agent_framework not in mcp_server_map:
-        msg = f"Unsupported agent type: {agent_framework}. Currently supported types are: {list(mcp_server_map.keys())}"
-        raise NotImplementedError(msg)
-
-    # Create the manager instance which will manage the MCP tool context
-    manager_class = mcp_server_map[agent_framework]
-    manager = manager_class(mcp_tool)
+    manager = get_mcp_server(mcp_tool, agent_framework)
     await manager.setup_tools()
 
     return manager

mozilla-ai/any-agent

src/any_agent/frameworks/any_agent.py
@@ -12,7 +12,7 @@ from any_agent.tracing import setup_tracing
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
-    from any_agent.tools.mcp import MCPServerBase
+    from any_agent.tools.mcp.mcp_server import MCPServerBase
 
 
 class AnyAgent(ABC):

src/any_agent/tools/mcp/frameworks/agno.py
@@ -1,0 +1,73 @@
+import os
+from abc import ABC, abstractmethod
+from contextlib import suppress
+from typing import Literal
+
+from any_agent.config import AgentFramework, MCPSseParams, MCPStdioParams
+from any_agent.tools.mcp.mcp_server import MCPServerBase
+
+mcp_available = False
+with suppress(ImportError):
+    from agno.tools.mcp import MCPTools as AgnoMCPTools
+    from mcp import ClientSession
+    from mcp.client.sse import sse_client
+
+    mcp_available = True
+
+
+class AgnoMCPServerBase(MCPServerBase, ABC):
+    server: AgnoMCPTools | None = None
+    framework: Literal[AgentFramework.AGNO] = AgentFramework.AGNO
+
+    def check_dependencies(self) -> None:
+        """Check if the required dependencies for the MCP server are available."""
+        self.libraries = "any-agent[mcp,agno]"
+        self.mcp_available = mcp_available
+        super().check_dependencies()
+
+    @abstractmethod
+    async def setup_tools(self) -> None:
+        """Set up the Agno MCP server with the provided configuration."""
+        if not self.server:
+            msg = "MCP server is not set up. Please call `setup` from a concrete class."
+            raise ValueError(msg)
+
+        self.tools = [await self._exit_stack.enter_async_context(self.server)]
+
+
+class AgnoMCPServerStdio(AgnoMCPServerBase):
+    mcp_tool: MCPStdioParams
+
+    async def setup_tools(self) -> None:
+        server_params = f"{self.mcp_tool.command} {' '.join(self.mcp_tool.args)}"
+        self.server = AgnoMCPTools(
+            command=server_params,
+            include_tools=list(self.mcp_tool.tools or []),
+            env={**os.environ},
+        )
+
+        await super().setup_tools()
+
+
+class AgnoMCPServerSse(AgnoMCPServerBase):
+    mcp_tool: MCPSseParams
+
+    async def setup_tools(self) -> None:
+        client = sse_client(
+            url=self.mcp_tool.url,
+            headers=self.mcp_tool.headers,
+        )
+        sse_transport = await self._exit_stack.enter_async_context(client)
+        stdio, write = sse_transport
+        client_session = ClientSession(stdio, write)
+        session = await self._exit_stack.enter_async_context(client_session)
+        await session.initialize()
+        self.server = AgnoMCPTools(
+            session=session,
+            include_tools=self.mcp_tool.tools,
+        )
+
+        await super().setup_tools()
+
+
+AgnoMCPServer = AgnoMCPServerStdio | AgnoMCPServerSse

src/any_agent/tools/mcp/frameworks/google.py
@@ -1,0 +1,70 @@
+import os
+from abc import ABC, abstractmethod
+from contextlib import suppress
+from typing import Literal
+
+from any_agent.config import AgentFramework, MCPSseParams, MCPStdioParams
+from any_agent.tools.mcp.mcp_server import MCPServerBase
+
+mcp_available = False
+with suppress(ImportError):
+    from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset as GoogleMCPToolset
+    from google.adk.tools.mcp_tool.mcp_toolset import (
+        SseServerParams as GoogleSseServerParameters,
+    )
+    from google.adk.tools.mcp_tool.mcp_toolset import (
+        StdioServerParameters as GoogleStdioServerParameters,
+    )
+
+    mcp_available = True
+
+
+class GoogleMCPServerBase(MCPServerBase, ABC):
+    server: GoogleMCPToolset | None = None
+    framework: Literal[AgentFramework.GOOGLE] = AgentFramework.GOOGLE
+
+    def check_dependencies(self) -> None:
+        """Check if the required dependencies for the MCP server are available."""
+        self.libraries = "any-agent[mcp,google]"
+        self.mcp_available = mcp_available
+        super().check_dependencies()
+
+    @abstractmethod
+    async def setup_tools(self) -> None:
+        """Set up the Google MCP server with the provided configuration."""
+        if not self.server:
+            msg = "MCP server is not set up. Please call `setup` from a concrete class."
+            raise ValueError(msg)
+
+        await self._exit_stack.enter_async_context(self.server)
+        self.tools = await self.server.load_tools()
+
+
+class GoogleMCPServerStdio(GoogleMCPServerBase):
+    mcp_tool: MCPStdioParams
+
+    async def setup_tools(self) -> None:
+        params = GoogleStdioServerParameters(
+            command=self.mcp_tool.command,
+            args=list(self.mcp_tool.args),
+            env={**os.environ},
+        )
+        self.server = GoogleMCPToolset(connection_params=params)
+
+        await super().setup_tools()
+
+
+class GoogleMCPServerSse(GoogleMCPServerBase):
+    mcp_tool: MCPSseParams
+
+    async def setup_tools(self) -> None:
+        params = GoogleSseServerParameters(
+            url=self.mcp_tool.url,
+            headers=dict(self.mcp_tool.headers or {}),
+        )
+        self.server = GoogleMCPToolset(connection_params=params)
+
+        await super().setup_tools()
+
+
+GoogleMCPServer = GoogleMCPServerStdio | GoogleMCPServerSse

src/any_agent/tools/mcp/frameworks/llama_index.py
@@ -1,0 +1,64 @@
+import os
+from abc import ABC, abstractmethod
+from contextlib import suppress
+from typing import Literal
+
+from any_agent.config import AgentFramework, MCPSseParams, MCPStdioParams
+from any_agent.tools.mcp.mcp_server import MCPServerBase
+
+mcp_available = False
+with suppress(ImportError):
+    from llama_index.tools.mcp import BasicMCPClient as LlamaIndexMCPClient
+    from llama_index.tools.mcp import McpToolSpec as LlamaIndexMcpToolSpec
+
+    mcp_available = True
+
+
+class LlamaIndexMCPServerBase(MCPServerBase, ABC):
+    client: LlamaIndexMCPClient | None = None
+    framework: Literal[AgentFramework.LLAMA_INDEX] = AgentFramework.LLAMA_INDEX
+
+    def check_dependencies(self) -> None:
+        """Check if the required dependencies for the MCP server are available."""
+        self.libraries = "any-agent[mcp,llama_index]"
+        self.mcp_available = mcp_available
+        super().check_dependencies()
+
+    @abstractmethod
+    async def setup_tools(self) -> None:
+        """Set up the LlamaIndex MCP server with the provided configuration."""
+        if not self.client:
+            msg = "MCP client is not set up. Please call `setup` from a concrete class."
+            raise ValueError(msg)
+
+        mcp_tool_spec = LlamaIndexMcpToolSpec(
+            client=self.client,
+            allowed_tools=list(self.mcp_tool.tools or []),
+        )
+
+        self.tools = await mcp_tool_spec.to_tool_list_async()
+
+
+class LlamaIndexMCPServerStdio(LlamaIndexMCPServerBase):
+    mcp_tool: MCPStdioParams
+
+    async def setup_tools(self) -> None:
+        self.client = LlamaIndexMCPClient(
+            command_or_url=self.mcp_tool.command,
+            args=list(self.mcp_tool.args),
+            env={**os.environ},
+        )
+
+        await super().setup_tools()
+
+
+class LlamaIndexMCPServerSse(LlamaIndexMCPServerBase):
+    mcp_tool: MCPSseParams
+
+    async def setup_tools(self) -> None:
+        self.client = LlamaIndexMCPClient(command_or_url=self.mcp_tool.url)
+
+        await super().setup_tools()
+
+
+LlamaIndexMCPServer = LlamaIndexMCPServerStdio | LlamaIndexMCPServerSse

src/any_agent/tools/mcp/frameworks/smolagents.py
@@ -1,0 +1,93 @@
+import os
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+from contextlib import suppress
+from textwrap import dedent
+from typing import Literal
+
+from any_agent.config import AgentFramework, MCPSseParams, MCPStdioParams
+from any_agent.logging import logger
+from any_agent.tools.mcp.mcp_server import MCPServerBase
+
+mcp_available = False
+with suppress(ImportError):
+    from mcp import StdioServerParameters
+    from smolagents.mcp_client import MCPClient
+    from smolagents.mcp_client import Tool as SmolagentsTool
+
+    mcp_available = True
+
+
+class SmolagentsMCPServerBase(MCPServerBase, ABC):
+    smolagent_tools: Sequence["SmolagentsTool"] | None = None
+    framework: Literal[AgentFramework.SMOLAGENTS] = AgentFramework.SMOLAGENTS
+
+    def check_dependencies(self) -> None:
+        """Check if the required dependencies for the MCP server are available."""
+        self.libraries = "any-agent[mcp,smolagents]"
+        self.mcp_available = mcp_available
+        super().check_dependencies()
+
+    @abstractmethod
+    async def setup_tools(self) -> None:
+        """Set up the Smolagents MCP server with the provided configuration."""
+        if not self.smolagent_tools:
+            msg = "Tool collection is not set up. Please call `setup` from a concrete class."
+            raise ValueError(msg)
+
+        # Only add the tools listed in mcp_tool['tools'] if specified
+        requested_tools = list(self.mcp_tool.tools or [])
+        if not requested_tools:
+            logger.info(
+                "No specific tools requested for MCP server, using all available tools:",
+            )
+            logger.info("Tools available: %s", self.smolagent_tools)
+            self.tools = self.smolagent_tools
+            return
+
+        filtered_tools = [
+            tool for tool in self.smolagent_tools if tool.name in requested_tools
+        ]
+        if len(filtered_tools) == len(requested_tools):
+            self.tools = filtered_tools
+            return
+
+        tool_names = [tool.name for tool in filtered_tools]
+        raise ValueError(
+            dedent(f"""Could not find all requested tools in the MCP server:
+                        Requested: {requested_tools}
+                        Set:   {tool_names}"""),
+        )
+
+
+class SmolagentsMCPServerStdio(SmolagentsMCPServerBase):
+    mcp_tool: MCPStdioParams
+
+    async def setup_tools(self) -> None:
+        server_parameters = StdioServerParameters(
+            command=self.mcp_tool.command,
+            args=list(self.mcp_tool.args),
+            env={**os.environ},
+        )
+        self.smolagent_tools = self._exit_stack.enter_context(
+            MCPClient(server_parameters)
+        )
+
+        await super().setup_tools()
+
+
+class SmolagentsMCPServerSse(SmolagentsMCPServerBase):
+    mcp_tool: MCPSseParams
+
+    async def setup_tools(self) -> None:
+        server_parameters = {
+            "url": self.mcp_tool.url,
+        }
+        self.smolagent_tools = self._exit_stack.enter_context(
+            MCPClient(server_parameters)
+        )
+
+        await super().setup_tools()
+
+
+SmolagentsMCPServer = SmolagentsMCPServerStdio | SmolagentsMCPServerSse

src/any_agent/tools/mcp/mcp_server.py
@@ -1,0 +1,34 @@
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+from contextlib import AsyncExitStack
+from typing import Any
+
+from pydantic import BaseModel, ConfigDict, Field, PrivateAttr
+
+from any_agent.config import AgentFramework, MCPParams, Tool
+
+
+class MCPServerBase(BaseModel, ABC):
+    mcp_tool: MCPParams
+    framework: AgentFramework
+    mcp_available: bool = False
+    libraries: str = ""
+
+    _exit_stack: AsyncExitStack = PrivateAttr(default_factory=AsyncExitStack)
+    tools: Sequence[Tool] = Field(default_factory=list)
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    def model_post_init(self, context: Any) -> None:
+        self.check_dependencies()
+
+    @abstractmethod
+    async def setup_tools(self) -> None: ...
+
+    @abstractmethod
+    def check_dependencies(self) -> None:
+        if self.mcp_available:
+            return
+
+        msg = f"You need to `pip install '{self.libraries}'` to use MCP."
+        raise ImportError(msg)

src/any_agent/tools/wrappers.py
@@ -4,14 +4,9 @@ from functools import wraps
 from typing import TYPE_CHECKING, Any
 
 from any_agent.config import AgentFramework, MCPParams, Tool
-from any_agent.tools.mcp import (
-    AgnoMCPServer,
-    GoogleMCPServer,
-    LangchainMCPServer,
-    LlamaIndexMCPServer,
+from any_agent.tools import (
     MCPServerBase,
-    OpenAIMCPServer,
-    SmolagentsMCPServer,
+    get_mcp_server,
 )
 
 if TYPE_CHECKING:
@@ -87,22 +82,7 @@ async def wrap_mcp_server(
     """Generic MCP server wrapper that can work with different frameworks
     based on the specified agent_framework
     """
-    # Select the appropriate manager based on agent_framework
-    mcp_server_map: dict[AgentFramework, type[MCPServerBase]] = {
-        AgentFramework.OPENAI: OpenAIMCPServer,
-        AgentFramework.SMOLAGENTS: SmolagentsMCPServer,
-        AgentFramework.LANGCHAIN: LangchainMCPServer,
-        AgentFramework.GOOGLE: GoogleMCPServer,
-        AgentFramework.LLAMA_INDEX: LlamaIndexMCPServer,
-        AgentFramework.AGNO: AgnoMCPServer,
-    }
-    if agent_framework not in mcp_server_map:
-        msg = f"Unsupported agent type: {agent_framework}. Currently supported types are: {list(mcp_server_map.keys())}"
-        raise NotImplementedError(msg)
-
-    # Create the manager instance which will manage the MCP tool context
-    manager_class = mcp_server_map[agent_framework]
-    manager = manager_class(mcp_tool)
+    manager = get_mcp_server(mcp_tool, agent_framework)
     await manager.setup_tools()
 
     return manager