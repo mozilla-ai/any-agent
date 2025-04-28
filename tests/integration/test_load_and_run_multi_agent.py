e2df324cc6dfc54e30b28d65e1782cfb2d8ccd03
Javier Torres <javier@mozilla.ai>
2025-04-28 11:12:17 +0200

    Check on TOOL traces, use more specific prompts

--- tests/integration/test_load_and_run_multi_agent.py
+++ tests/integration/test_load_and_run_multi_agent.py
@@ -2,19 +2,23 @@ import os
 from pathlib import Path
 
 import pytest
+import json
+
+from typing import Dict, Callable
 
 from any_agent import AgentConfig, AgentFramework, AnyAgent
 from any_agent.config import TracingConfig
-from any_agent.tools import search_web, visit_webpage
+from any_agent.tools import search_web, visit_webpage, show_final_answer
 from any_agent.tracing.trace import AgentTrace, is_tracing_supported
-
 
 @pytest.mark.skipif(
     os.environ.get("ANY_AGENT_INTEGRATION_TESTS", "FALSE").upper() != "TRUE",
     reason="Integration tests require `ANY_AGENT_INTEGRATION_TESTS=TRUE` env var",
 )
 def test_load_and_run_multi_agent(
-    agent_framework: AgentFramework, tmp_path: Path
+    agent_framework: AgentFramework,
+    check_multi_tool_usage: Callable[[Dict], None],
+    tmp_path: Path
 ) -> None:
     kwargs = {}
 
@@ -23,7 +27,10 @@ def test_load_and_run_multi_agent(
             f"Skipping test for {agent_framework.name} because it does not support multi-agent"
         )
 
-    kwargs["model_id"] = "gpt-4.1-nano"
+    agent_model = "gpt-4.1-nano"
+    # kwargs["model_id"] = "gemini/gemini-2.0-flash-lite"
+    # kwargs["model_id"] = "gemini/gemini-2.5-flash-preview-04-17"
+    kwargs["model_id"] = agent_model
     if "OPENAI_API_KEY" not in os.environ:
         pytest.skip(f"OPENAI_API_KEY needed for {agent_framework.name}")
 
@@ -42,20 +49,19 @@ def test_load_and_run_multi_agent(
     managed_agents = [
         AgentConfig(
             name="search_web_agent",
-            model_id="gpt-4.1-nano",
-            description="Agent that can search the web",
+            model_id=agent_model,
+            description="Agent that can search the web. It can find answers on the web if the query cannot be answered. Use this tool if additional information would be needed to answer the query.",
             tools=[search_web],
             model_args=model_args,
         ),
         AgentConfig(
             name="visit_webpage_agent",
-            model_id="gpt-4.1-nano",
+            model_id=agent_model,
             description="Agent that can visit webpages",
             tools=[visit_webpage],
             model_args=model_args,
         ),
     ]
-
     traces = tmp_path / "traces"
     agent = AnyAgent.create(
         agent_framework=agent_framework,
@@ -65,25 +71,9 @@ def test_load_and_run_multi_agent(
             output_dir=str(traces), console=False, save=True, cost_info=True
         ),
     )
-    agent_trace = agent.run("Which agent framework is the best?")
-
-    assert agent_trace
-    assert agent_trace.final_output
-    if is_tracing_supported(agent_framework):
-        assert agent_trace.spans
-        assert len(agent_trace.spans) > 0
-        assert traces.exists()
-        trace_files = [str(x) for x in traces.iterdir()]
-        assert agent_trace.output_file in trace_files
-        assert agent_framework.name in agent_trace.output_file
-        cost_sum = agent_trace.get_total_cost()
-        assert cost_sum.total_cost > 0
-        assert cost_sum.total_cost < 1.00
-        assert cost_sum.total_tokens > 0
-        assert cost_sum.total_tokens < 20000
 
     try:
-        agent_trace = agent.run("Which agent framework is the best?")
+        agent_trace = agent.run("Which LLM agent framework is the most appropriate to execute SQL queries using grammar constrained decoding? I am working on a business environment with my own premises, and I would prefer hosting an open source model myself.")
 
         assert isinstance(agent_trace, AgentTrace)
         assert agent_trace.final_output
