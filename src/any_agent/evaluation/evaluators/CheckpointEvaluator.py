21ac00a91cd28614f32e0e73d2324e657679efa1
Nathan Brake <nathan@mozilla.ai>
2025-05-07 12:17:13 -0400

    downgrade a log statement to debug

--- src/any_agent/evaluation/evaluators/CheckpointEvaluator.py
+++ src/any_agent/evaluation/evaluators/CheckpointEvaluator.py
@@ -33,7 +33,7 @@ class CheckpointEvaluator(LLMEvaluator):
         """
         evidence = processor.extract_evidence(trace)
         evidence = evidence.replace("<", "\\<").replace(">", "\\>")
-        logger.info(f"""Evidence\n{evidence}\n""")
+        logger.debug(f"""Evidence\n{evidence}\n""")
         results = []
 
         for checkpoint in checkpoints:
