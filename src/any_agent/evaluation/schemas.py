flowportal-ai/belt

Simplify Evaluation Code (#224)
* remove uneccesary "extract hypothesis answer" and simplify the class based arch

* lint

* spelling

src/any_agent/evaluation/schemas.py
@@ -1,0 +1,67 @@
+from collections.abc import Sequence
+
+from pydantic import BaseModel, ConfigDict
+from typing_extensions import TypedDict
+
+from flow_portal.tracing.trace import AgentTrace
+
+
+class EvaluationResult(BaseModel):
+    """Represents the result of evaluating a criterion."""
+
+    model_config = ConfigDict(extra="forbid")
+    passed: bool
+    reason: str
+    criteria: str
+    points: int
+
+
+class AnswerDetails(TypedDict):
+    answer_start: Sequence[int]
+    text: Sequence[str]
+
+
+class GroundTruthAnswers(TypedDict):
+    id: str
+    answers: AnswerDetails
+
+
+class CheckpointCriteria(BaseModel):
+    """Represents a checkpoint criteria with a description."""
+
+    model_config = ConfigDict(extra="forbid")
+    criteria: str
+    points: int
+
+
+class GroundTruthAnswer(TypedDict):
+    name: str
+    value: float
+    points: float
+
+
+class TraceEvaluationResult(BaseModel):
+    """Represents the result of evaluating a trace."""
+
+    model_config = ConfigDict(extra="forbid")
+
+    trace: AgentTrace
+    hypothesis_answer: str | None
+    checkpoint_results: list[EvaluationResult]
+    hypothesis_answer_results: list[EvaluationResult]
+    direct_results: list[EvaluationResult]
+
+    @property
+    def score(self) -> float:
+        """Calculate the score based on the evaluation results."""
+        all_results = (
+            self.checkpoint_results
+            + self.hypothesis_answer_results
+            + self.direct_results
+        )
+        total_points = sum([result.points for result in all_results])
+        if total_points == 0:
+            msg = "Total points is 0, cannot calculate score."
+            raise ValueError(msg)
+        passed_points = sum([result.points for result in all_results if result.passed])
+        return float(passed_points / total_points)

src/any_agent/tracing/processors/langchain.py
@@ -1,14 +1,12 @@
 import json
 from typing import TYPE_CHECKING, Any
 
-from langchain_core.messages import BaseMessage
-
 from flow_portal import AgentFramework
 from flow_portal.tracing import TracingProcessor
 from flow_portal.tracing.otel_types import StatusCode
 
 if TYPE_CHECKING:
-    from flow_portal.tracing.trace import AgentSpan, AgentTrace
+    from flow_portal.tracing.trace import AgentSpan
 
 
 class LangchainTracingProcessor(TracingProcessor):
@@ -16,27 +14,6 @@ class LangchainTracingProcessor(TracingProcessor):
 
     def _get_agent_framework(self) -> AgentFramework:
         return AgentFramework.LANGCHAIN
-
-    def _extract_hypothesis_answer(self, trace: "AgentTrace") -> str:
-        for span in reversed(trace.spans):
-            if span.attributes["openinference.span.kind"] == "AGENT":
-                content = span.attributes["output.value"]
-                # Extract content from serialized langchain message
-                message = json.loads(content)["messages"][0]
-                message = self.parse_generic_key_value_string(message)
-                base_message = BaseMessage(content=message["content"], type="AGENT")
-                # Use the interpreted string for printing
-                final_text = base_message.text()
-                # Either decode escape sequences if they're present
-                try:
-                    final_text = final_text.encode().decode("unicode_escape")
-                except UnicodeDecodeError:
-                    # If that fails, the escape sequences might already be interpreted
-                    pass
-                return final_text
-
-        msg = "No agent final answer found in trace"
-        raise ValueError(msg)
 
     def _extract_llm_interaction(self, span: "AgentSpan") -> dict[str, Any]:
         """Extract LLM interaction details from a span."""

src/any_agent/tracing/processors/llama_index.py
@@ -3,11 +3,10 @@ import json
 from typing import TYPE_CHECKING, Any
 
 from flow_portal import AgentFramework
-from flow_portal.logging import logger
 from flow_portal.tracing.processors.base import TracingProcessor
 
 if TYPE_CHECKING:
-    from flow_portal.tracing.trace import AgentSpan, AgentTrace
+    from flow_portal.tracing.trace import AgentSpan
 
 
 class LlamaIndexTracingProcessor(TracingProcessor):
@@ -15,18 +14,6 @@ class LlamaIndexTracingProcessor(TracingProcessor):
 
     def _get_agent_framework(self) -> AgentFramework:
         return AgentFramework.LLAMA_INDEX
-
-    def _extract_hypothesis_answer(self, trace: "AgentTrace") -> str:
-        for span in reversed(trace.spans):
-            # Looking for the final response that has the summary answer
-            if span.attributes.get("openinference.span.kind") == "LLM":
-                output_key = (
-                    "llm.output_messages.0.message.contents.0.message_content.text"
-                )
-                if output_key in span.attributes:
-                    return str(span.attributes[output_key])
-        logger.warning("No agent final answer found in trace")
-        return "NO FINAL ANSWER FOUND"
 
     def _extract_llm_interaction(self, span: "AgentSpan") -> dict[str, Any]:
         attributes = span.attributes

src/any_agent/tracing/processors/smolagents.py
@@ -6,7 +6,7 @@ from flow_portal.tracing.otel_types import StatusCode
 from flow_portal.tracing.processors.base import TracingProcessor
 
 if TYPE_CHECKING:
-    from flow_portal.tracing.trace import AgentSpan, AgentTrace
+    from flow_portal.tracing.trace import AgentSpan
 
 
 class SmolagentsTracingProcessor(TracingProcessor):
@@ -14,14 +14,6 @@ class SmolagentsTracingProcessor(TracingProcessor):
 
     def _get_agent_framework(self) -> AgentFramework:
         return AgentFramework.SMOLAGENTS
-
-    def _extract_hypothesis_answer(self, trace: "AgentTrace") -> str:
-        for span in reversed(trace.spans):
-            if span.attributes["openinference.span.kind"] == "AGENT":
-                return str(span.attributes["output.value"])
-
-        msg = "No agent final answer found in trace"
-        raise ValueError(msg)
 
     def _extract_llm_interaction(self, span: "AgentSpan") -> dict[str, Any]:
         """Extract LLM interaction details from a span."""

tests/unit/evaluation/test_evaluate.py
@@ -4,7 +4,7 @@ import pytest
 
 from flow_portal.config import AgentFramework
 from flow_portal.evaluation import EvaluationCase, TraceEvaluationResult, evaluate
-from flow_portal.evaluation.evaluators.schemas import (
+from flow_portal.evaluation.schemas import (
     EvaluationResult,
 )
 from flow_portal.tracing.trace import AgentTrace
@@ -13,66 +13,93 @@ from flow_portal.tracing.trace import AgentTrace
 def test_evaluate_runs_all_evaluators(
     evaluation_case: EvaluationCase, agent_trace: AgentTrace
 ) -> None:
-    """This unit test is designed to ensure that the EvaluationRunner properly iterates over all the traces and eval cases"""
+    """This unit test checks that all evaluators are called when evaluating a trace."""
     #### Set up the mocks for the evaluators so that we don't actually call LLMs.
-    mock_checkpoint_evaluator = MagicMock()
-    mock_hypothesis_evaluator = MagicMock()
-    mock_qa_evaluator = MagicMock()
+    mock_checkpoint_evaluate = MagicMock()
+    mock_hypothesis_evaluate = MagicMock()
+    mock_qa_evaluate = MagicMock()
 
-    ### Every evaluator will return the same result
+    ### Every evaluate will return the same result
     eval_result = [
         EvaluationResult(
             criteria="test criteria", passed=True, reason="test passed", points=1
         )
     ]
-    for evaluator in [
-        mock_checkpoint_evaluator,
-        mock_hypothesis_evaluator,
-        mock_qa_evaluator,
+    for evaluate_fn in [
+        mock_checkpoint_evaluate,
+        mock_hypothesis_evaluate,
+        mock_qa_evaluate,
     ]:
-        evaluator.evaluate.return_value = eval_result
+        evaluate_fn.return_value = eval_result
 
     with (
         patch(
-            "flow_portal.evaluation.evaluate.CheckpointEvaluator"
-        ) as mock_checkpoint_eval_cls,
+            "flow_portal.evaluation.evaluate.evaluate_checkpoint",
+            mock_checkpoint_evaluate,
+        ),
         patch(
-            "flow_portal.evaluation.evaluate.HypothesisEvaluator"
-        ) as mock_hypothesis_eval_cls,
-        patch(
-            "flow_portal.evaluation.evaluate.QuestionAnsweringSquadEvaluator"
-        ) as mock_qa_eval_cls,
-        patch(
-            "flow_portal.evaluation.evaluate.TracingProcessor.create"
-        ) as mock_processor_create,
+            "flow_portal.evaluation.evaluate.evaluate_hypothesis",
+            mock_hypothesis_evaluate,
+        ),
+        patch("flow_portal.evaluation.evaluate.evaluate_qa_squad", mock_qa_evaluate),
+        patch("flow_portal.evaluation.evaluate.TracingProcessor.create"),
     ):
-        # Evaluators are created for each evaluation case (but not for each trace)
-        mock_checkpoint_eval_cls.side_effect = [
-            mock_checkpoint_evaluator,
-        ]
-        mock_hypothesis_eval_cls.side_effect = [mock_hypothesis_evaluator]
-        mock_qa_eval_cls.side_effect = [mock_qa_evaluator]
-
-        # Mock processor is used to extract the hypothesis answer
-        mock_processor = MagicMock()
-        mock_processor._extract_hypothesis_answer.return_value = (
-            "Mock hypothesis answer"
-        )
-        mock_processor_create.return_value = mock_processor
-
         evaluate(
             evaluation_case=evaluation_case,
             trace=agent_trace,
             agent_framework=AgentFramework.OPENAI,
         )
 
-        assert mock_checkpoint_eval_cls.call_count == 1
-        assert mock_hypothesis_eval_cls.call_count == 1
-        assert mock_qa_eval_cls.call_count == 1
+        assert mock_checkpoint_evaluate.call_count == 1
+        assert mock_hypothesis_evaluate.call_count == 1
+        assert mock_qa_evaluate.call_count == 1
 
-        assert mock_checkpoint_evaluator.evaluate.call_count == 1
-        assert mock_hypothesis_evaluator.evaluate.call_count == 1
-        assert mock_qa_evaluator.evaluate.call_count == 1
+
+def test_evaluate_when_no_final_output(
+    evaluation_case: EvaluationCase, agent_trace: AgentTrace
+) -> None:
+    """This unit test checks that the hypothesis and qa evaluators are not called when there is no final output."""
+    #### Set up the mocks for the evaluators so that we don't actually call LLMs.
+    mock_checkpoint_evaluate = MagicMock()
+    mock_hypothesis_evaluate = MagicMock()
+    mock_qa_evaluate = MagicMock()
+
+    agent_trace.final_output = None
+
+    ### Every evaluate will return the same result
+    eval_result = [
+        EvaluationResult(
+            criteria="test criteria", passed=True, reason="test passed", points=1
+        )
+    ]
+    for evaluate_fn in [
+        mock_checkpoint_evaluate,
+        mock_hypothesis_evaluate,
+        mock_qa_evaluate,
+    ]:
+        evaluate_fn.return_value = eval_result
+
+    with (
+        patch(
+            "flow_portal.evaluation.evaluate.evaluate_checkpoint",
+            mock_checkpoint_evaluate,
+        ),
+        patch(
+            "flow_portal.evaluation.evaluate.evaluate_hypothesis",
+            mock_hypothesis_evaluate,
+        ),
+        patch("flow_portal.evaluation.evaluate.evaluate_qa_squad", mock_qa_evaluate),
+        patch("flow_portal.evaluation.evaluate.TracingProcessor.create"),
+    ):
+        evaluate(
+            evaluation_case=evaluation_case,
+            trace=agent_trace,
+            agent_framework=AgentFramework.OPENAI,
+        )
+
+        assert mock_checkpoint_evaluate.call_count == 1
+        assert mock_hypothesis_evaluate.call_count == 0
+        assert mock_qa_evaluate.call_count == 0
 
 
 def test_trace_evaluation_result_score_calculation(agent_trace: AgentTrace) -> None:

mozilla-ai/any-agent

src/any_agent/evaluation/schemas.py
@@ -1,0 +1,67 @@
+from collections.abc import Sequence
+
+from pydantic import BaseModel, ConfigDict
+from typing_extensions import TypedDict
+
+from any_agent.tracing.trace import AgentTrace
+
+
+class EvaluationResult(BaseModel):
+    """Represents the result of evaluating a criterion."""
+
+    model_config = ConfigDict(extra="forbid")
+    passed: bool
+    reason: str
+    criteria: str
+    points: int
+
+
+class AnswerDetails(TypedDict):
+    answer_start: Sequence[int]
+    text: Sequence[str]
+
+
+class GroundTruthAnswers(TypedDict):
+    id: str
+    answers: AnswerDetails
+
+
+class CheckpointCriteria(BaseModel):
+    """Represents a checkpoint criteria with a description."""
+
+    model_config = ConfigDict(extra="forbid")
+    criteria: str
+    points: int
+
+
+class GroundTruthAnswer(TypedDict):
+    name: str
+    value: float
+    points: float
+
+
+class TraceEvaluationResult(BaseModel):
+    """Represents the result of evaluating a trace."""
+
+    model_config = ConfigDict(extra="forbid")
+
+    trace: AgentTrace
+    hypothesis_answer: str | None
+    checkpoint_results: list[EvaluationResult]
+    hypothesis_answer_results: list[EvaluationResult]
+    direct_results: list[EvaluationResult]
+
+    @property
+    def score(self) -> float:
+        """Calculate the score based on the evaluation results."""
+        all_results = (
+            self.checkpoint_results
+            + self.hypothesis_answer_results
+            + self.direct_results
+        )
+        total_points = sum([result.points for result in all_results])
+        if total_points == 0:
+            msg = "Total points is 0, cannot calculate score."
+            raise ValueError(msg)
+        passed_points = sum([result.points for result in all_results if result.passed])
+        return float(passed_points / total_points)

src/any_agent/tracing/processors/langchain.py
@@ -1,14 +1,12 @@
 import json
 from typing import TYPE_CHECKING, Any
 
-from langchain_core.messages import BaseMessage
-
 from any_agent import AgentFramework
 from any_agent.tracing import TracingProcessor
 from any_agent.tracing.otel_types import StatusCode
 
 if TYPE_CHECKING:
-    from any_agent.tracing.trace import AgentSpan, AgentTrace
+    from any_agent.tracing.trace import AgentSpan
 
 
 class LangchainTracingProcessor(TracingProcessor):
@@ -16,27 +14,6 @@ class LangchainTracingProcessor(TracingProcessor):
 
     def _get_agent_framework(self) -> AgentFramework:
         return AgentFramework.LANGCHAIN
-
-    def _extract_hypothesis_answer(self, trace: "AgentTrace") -> str:
-        for span in reversed(trace.spans):
-            if span.attributes["openinference.span.kind"] == "AGENT":
-                content = span.attributes["output.value"]
-                # Extract content from serialized langchain message
-                message = json.loads(content)["messages"][0]
-                message = self.parse_generic_key_value_string(message)
-                base_message = BaseMessage(content=message["content"], type="AGENT")
-                # Use the interpreted string for printing
-                final_text = base_message.text()
-                # Either decode escape sequences if they're present
-                try:
-                    final_text = final_text.encode().decode("unicode_escape")
-                except UnicodeDecodeError:
-                    # If that fails, the escape sequences might already be interpreted
-                    pass
-                return final_text
-
-        msg = "No agent final answer found in trace"
-        raise ValueError(msg)
 
     def _extract_llm_interaction(self, span: "AgentSpan") -> dict[str, Any]:
         """Extract LLM interaction details from a span."""

src/any_agent/tracing/processors/llama_index.py
@@ -3,11 +3,10 @@ import json
 from typing import TYPE_CHECKING, Any
 
 from any_agent import AgentFramework
-from any_agent.logging import logger
 from any_agent.tracing.processors.base import TracingProcessor
 
 if TYPE_CHECKING:
-    from any_agent.tracing.trace import AgentSpan, AgentTrace
+    from any_agent.tracing.trace import AgentSpan
 
 
 class LlamaIndexTracingProcessor(TracingProcessor):
@@ -15,18 +14,6 @@ class LlamaIndexTracingProcessor(TracingProcessor):
 
     def _get_agent_framework(self) -> AgentFramework:
         return AgentFramework.LLAMA_INDEX
-
-    def _extract_hypothesis_answer(self, trace: "AgentTrace") -> str:
-        for span in reversed(trace.spans):
-            # Looking for the final response that has the summary answer
-            if span.attributes.get("openinference.span.kind") == "LLM":
-                output_key = (
-                    "llm.output_messages.0.message.contents.0.message_content.text"
-                )
-                if output_key in span.attributes:
-                    return str(span.attributes[output_key])
-        logger.warning("No agent final answer found in trace")
-        return "NO FINAL ANSWER FOUND"
 
     def _extract_llm_interaction(self, span: "AgentSpan") -> dict[str, Any]:
         attributes = span.attributes

src/any_agent/tracing/processors/smolagents.py
@@ -6,7 +6,7 @@ from any_agent.tracing.otel_types import StatusCode
 from any_agent.tracing.processors.base import TracingProcessor
 
 if TYPE_CHECKING:
-    from any_agent.tracing.trace import AgentSpan, AgentTrace
+    from any_agent.tracing.trace import AgentSpan
 
 
 class SmolagentsTracingProcessor(TracingProcessor):
@@ -14,14 +14,6 @@ class SmolagentsTracingProcessor(TracingProcessor):
 
     def _get_agent_framework(self) -> AgentFramework:
         return AgentFramework.SMOLAGENTS
-
-    def _extract_hypothesis_answer(self, trace: "AgentTrace") -> str:
-        for span in reversed(trace.spans):
-            if span.attributes["openinference.span.kind"] == "AGENT":
-                return str(span.attributes["output.value"])
-
-        msg = "No agent final answer found in trace"
-        raise ValueError(msg)
 
     def _extract_llm_interaction(self, span: "AgentSpan") -> dict[str, Any]:
         """Extract LLM interaction details from a span."""

tests/unit/evaluation/test_evaluate.py
@@ -4,7 +4,7 @@ import pytest
 
 from any_agent.config import AgentFramework
 from any_agent.evaluation import EvaluationCase, TraceEvaluationResult, evaluate
-from any_agent.evaluation.evaluators.schemas import (
+from any_agent.evaluation.schemas import (
     EvaluationResult,
 )
 from any_agent.tracing.trace import AgentTrace
@@ -13,66 +13,93 @@ from any_agent.tracing.trace import AgentTrace
 def test_evaluate_runs_all_evaluators(
     evaluation_case: EvaluationCase, agent_trace: AgentTrace
 ) -> None:
-    """This unit test is designed to ensure that the EvaluationRunner properly iterates over all the traces and eval cases"""
+    """This unit test checks that all evaluators are called when evaluating a trace."""
     #### Set up the mocks for the evaluators so that we don't actually call LLMs.
-    mock_checkpoint_evaluator = MagicMock()
-    mock_hypothesis_evaluator = MagicMock()
-    mock_qa_evaluator = MagicMock()
+    mock_checkpoint_evaluate = MagicMock()
+    mock_hypothesis_evaluate = MagicMock()
+    mock_qa_evaluate = MagicMock()
 
-    ### Every evaluator will return the same result
+    ### Every evaluate will return the same result
     eval_result = [
         EvaluationResult(
             criteria="test criteria", passed=True, reason="test passed", points=1
         )
     ]
-    for evaluator in [
-        mock_checkpoint_evaluator,
-        mock_hypothesis_evaluator,
-        mock_qa_evaluator,
+    for evaluate_fn in [
+        mock_checkpoint_evaluate,
+        mock_hypothesis_evaluate,
+        mock_qa_evaluate,
     ]:
-        evaluator.evaluate.return_value = eval_result
+        evaluate_fn.return_value = eval_result
 
     with (
         patch(
-            "any_agent.evaluation.evaluate.CheckpointEvaluator"
-        ) as mock_checkpoint_eval_cls,
+            "any_agent.evaluation.evaluate.evaluate_checkpoint",
+            mock_checkpoint_evaluate,
+        ),
         patch(
-            "any_agent.evaluation.evaluate.HypothesisEvaluator"
-        ) as mock_hypothesis_eval_cls,
-        patch(
-            "any_agent.evaluation.evaluate.QuestionAnsweringSquadEvaluator"
-        ) as mock_qa_eval_cls,
-        patch(
-            "any_agent.evaluation.evaluate.TracingProcessor.create"
-        ) as mock_processor_create,
+            "any_agent.evaluation.evaluate.evaluate_hypothesis",
+            mock_hypothesis_evaluate,
+        ),
+        patch("any_agent.evaluation.evaluate.evaluate_qa_squad", mock_qa_evaluate),
+        patch("any_agent.evaluation.evaluate.TracingProcessor.create"),
     ):
-        # Evaluators are created for each evaluation case (but not for each trace)
-        mock_checkpoint_eval_cls.side_effect = [
-            mock_checkpoint_evaluator,
-        ]
-        mock_hypothesis_eval_cls.side_effect = [mock_hypothesis_evaluator]
-        mock_qa_eval_cls.side_effect = [mock_qa_evaluator]
-
-        # Mock processor is used to extract the hypothesis answer
-        mock_processor = MagicMock()
-        mock_processor._extract_hypothesis_answer.return_value = (
-            "Mock hypothesis answer"
-        )
-        mock_processor_create.return_value = mock_processor
-
         evaluate(
             evaluation_case=evaluation_case,
             trace=agent_trace,
             agent_framework=AgentFramework.OPENAI,
         )
 
-        assert mock_checkpoint_eval_cls.call_count == 1
-        assert mock_hypothesis_eval_cls.call_count == 1
-        assert mock_qa_eval_cls.call_count == 1
+        assert mock_checkpoint_evaluate.call_count == 1
+        assert mock_hypothesis_evaluate.call_count == 1
+        assert mock_qa_evaluate.call_count == 1
 
-        assert mock_checkpoint_evaluator.evaluate.call_count == 1
-        assert mock_hypothesis_evaluator.evaluate.call_count == 1
-        assert mock_qa_evaluator.evaluate.call_count == 1
+
+def test_evaluate_when_no_final_output(
+    evaluation_case: EvaluationCase, agent_trace: AgentTrace
+) -> None:
+    """This unit test checks that the hypothesis and qa evaluators are not called when there is no final output."""
+    #### Set up the mocks for the evaluators so that we don't actually call LLMs.
+    mock_checkpoint_evaluate = MagicMock()
+    mock_hypothesis_evaluate = MagicMock()
+    mock_qa_evaluate = MagicMock()
+
+    agent_trace.final_output = None
+
+    ### Every evaluate will return the same result
+    eval_result = [
+        EvaluationResult(
+            criteria="test criteria", passed=True, reason="test passed", points=1
+        )
+    ]
+    for evaluate_fn in [
+        mock_checkpoint_evaluate,
+        mock_hypothesis_evaluate,
+        mock_qa_evaluate,
+    ]:
+        evaluate_fn.return_value = eval_result
+
+    with (
+        patch(
+            "any_agent.evaluation.evaluate.evaluate_checkpoint",
+            mock_checkpoint_evaluate,
+        ),
+        patch(
+            "any_agent.evaluation.evaluate.evaluate_hypothesis",
+            mock_hypothesis_evaluate,
+        ),
+        patch("any_agent.evaluation.evaluate.evaluate_qa_squad", mock_qa_evaluate),
+        patch("any_agent.evaluation.evaluate.TracingProcessor.create"),
+    ):
+        evaluate(
+            evaluation_case=evaluation_case,
+            trace=agent_trace,
+            agent_framework=AgentFramework.OPENAI,
+        )
+
+        assert mock_checkpoint_evaluate.call_count == 1
+        assert mock_hypothesis_evaluate.call_count == 0
+        assert mock_qa_evaluate.call_count == 0
 
 
 def test_trace_evaluation_result_score_calculation(agent_trace: AgentTrace) -> None: