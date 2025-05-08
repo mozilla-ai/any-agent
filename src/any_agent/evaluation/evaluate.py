flowportal-ai/belt

TraceEvaluationResult doesn't belong in evaluator schema class (#222)
* traceresult doesn't belong in evaluator schema class

* lint

src/any_agent/evaluation/evaluate.py
@@ -1,3 +1,5 @@
+from pydantic import BaseModel, ConfigDict
+
 from flow_portal.config import AgentFramework
 from flow_portal.evaluation.evaluation_case import EvaluationCase
 from flow_portal.evaluation.evaluators import (
@@ -5,9 +7,36 @@ from flow_portal.evaluation.evaluators import (
     HypothesisEvaluator,
     QuestionAnsweringSquadEvaluator,
 )
-from flow_portal.evaluation.evaluators.schemas import TraceEvaluationResult
+from flow_portal.evaluation.evaluators.schemas import EvaluationResult
 from flow_portal.tracing import TracingProcessor
 from flow_portal.tracing.trace import AgentTrace
+
+
+class TraceEvaluationResult(BaseModel):
+    """Represents the result of evaluating a trace."""
+
+    model_config = ConfigDict(extra="forbid")
+
+    trace: AgentTrace
+    hypothesis_answer: str
+    checkpoint_results: list[EvaluationResult]
+    hypothesis_answer_results: list[EvaluationResult]
+    direct_results: list[EvaluationResult]
+
+    @property
+    def score(self) -> float:
+        """Calculate the score based on the evaluation results."""
+        all_results = (
+            self.checkpoint_results
+            + self.hypothesis_answer_results
+            + self.direct_results
+        )
+        total_points = sum([result.points for result in all_results])
+        if total_points == 0:
+            msg = "Total points is 0, cannot calculate score."
+            raise ValueError(msg)
+        passed_points = sum([result.points for result in all_results if result.passed])
+        return float(passed_points / total_points)
 
 
 def evaluate(

tests/docs/test_all.py
@@ -4,7 +4,7 @@ from unittest.mock import AsyncMock, MagicMock, patch
 import pytest
 from mktestdocs import check_md_file
 
-from flow_portal.evaluation.evaluators.schemas import TraceEvaluationResult
+from flow_portal.evaluation import TraceEvaluationResult
 
 
 # Note the use of `str`, makes for pretty output

tests/unit/evaluation/test_evaluate.py
@@ -3,10 +3,9 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from flow_portal.config import AgentFramework
-from flow_portal.evaluation import EvaluationCase, evaluate
+from flow_portal.evaluation import EvaluationCase, TraceEvaluationResult, evaluate
 from flow_portal.evaluation.evaluators.schemas import (
     EvaluationResult,
-    TraceEvaluationResult,
 )
 from flow_portal.tracing.trace import AgentTrace
 

mozilla-ai/any-agent

src/any_agent/evaluation/evaluate.py
@@ -1,3 +1,5 @@
+from pydantic import BaseModel, ConfigDict
+
 from any_agent.config import AgentFramework
 from any_agent.evaluation.evaluation_case import EvaluationCase
 from any_agent.evaluation.evaluators import (
@@ -5,9 +7,36 @@ from any_agent.evaluation.evaluators import (
     HypothesisEvaluator,
     QuestionAnsweringSquadEvaluator,
 )
-from any_agent.evaluation.evaluators.schemas import TraceEvaluationResult
+from any_agent.evaluation.evaluators.schemas import EvaluationResult
 from any_agent.tracing import TracingProcessor
 from any_agent.tracing.trace import AgentTrace
+
+
+class TraceEvaluationResult(BaseModel):
+    """Represents the result of evaluating a trace."""
+
+    model_config = ConfigDict(extra="forbid")
+
+    trace: AgentTrace
+    hypothesis_answer: str
+    checkpoint_results: list[EvaluationResult]
+    hypothesis_answer_results: list[EvaluationResult]
+    direct_results: list[EvaluationResult]
+
+    @property
+    def score(self) -> float:
+        """Calculate the score based on the evaluation results."""
+        all_results = (
+            self.checkpoint_results
+            + self.hypothesis_answer_results
+            + self.direct_results
+        )
+        total_points = sum([result.points for result in all_results])
+        if total_points == 0:
+            msg = "Total points is 0, cannot calculate score."
+            raise ValueError(msg)
+        passed_points = sum([result.points for result in all_results if result.passed])
+        return float(passed_points / total_points)
 
 
 def evaluate(

tests/docs/test_all.py
@@ -4,7 +4,7 @@ from unittest.mock import AsyncMock, MagicMock, patch
 import pytest
 from mktestdocs import check_md_file
 
-from any_agent.evaluation.evaluators.schemas import TraceEvaluationResult
+from any_agent.evaluation import TraceEvaluationResult
 
 
 # Note the use of `str`, makes for pretty output

tests/unit/evaluation/test_evaluate.py
@@ -3,10 +3,9 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from any_agent.config import AgentFramework
-from any_agent.evaluation import EvaluationCase, evaluate
+from any_agent.evaluation import EvaluationCase, TraceEvaluationResult, evaluate
 from any_agent.evaluation.evaluators.schemas import (
     EvaluationResult,
-    TraceEvaluationResult,
 )
 from any_agent.tracing.trace import AgentTrace
 