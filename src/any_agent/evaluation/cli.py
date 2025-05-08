flowportal-ai/belt

Simplify Evaluation Code (#220)
* clean up and simplify eval code

* downgrade a log statement to debug

* lint

src/any_agent/evaluation/cli.py
@@ -1,42 +1,42 @@
 import json
+import os
 
 from fire import Fire
 
 from flow_portal.config import AgentFramework
-from flow_portal.evaluation import EvaluationRunner
-from flow_portal.evaluation.evaluation_case import EvaluationCase
+from flow_portal.evaluation import EvaluationCase, evaluate
 from flow_portal.logging import logger
 from flow_portal.tracing.trace import AgentSpan, AgentTrace
 
 
 def do_eval(
-    evaluation_case_paths: list[str],
-    trace_paths: list[str],
+    evaluation_case_path: str,
+    trace_path: str,
     agent_framework: AgentFramework,
     output_path: str = "output/results.json",
 ) -> None:
     logger.info("Starting evaluation...")
-    runner = EvaluationRunner(output_path=output_path)
+    logger.info("Loading test case from %s", evaluation_case_path)
+    evaluation_case = EvaluationCase.from_yaml(evaluation_case_path)
+    logger.info("Loading tracing from %s", trace_path)
+    with open(trace_path, encoding="utf-8") as f:
+        spans = json.loads(f.read())
+    spans = [AgentSpan.model_validate_json(span) for span in spans]
+    trace = AgentTrace(spans=spans)
+    # dump the trace to a file
+    with open("tmp.tmp", "w", encoding="utf-8") as f:
+        f.write(trace.model_dump_json(indent=2))
+    result = evaluate(
+        evaluation_case=evaluation_case,
+        trace=trace,
+        agent_framework=agent_framework,
+    )
+    logger.info(f"Final score: {result.score}")
 
-    for evaluation_case_path in evaluation_case_paths:
-        logger.info("Loading test case from %s", evaluation_case_path)
-        evaluation_case = EvaluationCase.from_yaml(evaluation_case_path)
-        runner.add_evaluation_case(evaluation_case)
-
-    for trace_path in trace_paths:
-        logger.info("Loading tracing from %s", trace_path)
-        with open(trace_path, encoding="utf-8") as f:
-            spans = json.loads(f.read())
-        spans = [AgentSpan.model_validate_json(span) for span in spans]
-        trace = AgentTrace(spans=spans)
-        # dump the trace to a file
-        with open("tmp.tmp", "w", encoding="utf-8") as f:
-            f.write(trace.model_dump_json(indent=2))
-        runner.add_trace(trace, agent_framework)
-
-    logger.info("Running evaluation...")
-    runner.run()
-    logger.info("Evaluation completed.")
+    logger.info("Writing results to %s", output_path)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(result.model_dump_json(indent=2))
 
 
 def main() -> None:

src/any_agent/evaluation/evaluate.py
@@ -1,0 +1,51 @@
+from flow_portal.config import AgentFramework
+from flow_portal.evaluation.evaluation_case import EvaluationCase
+from flow_portal.evaluation.evaluators import (
+    CheckpointEvaluator,
+    HypothesisEvaluator,
+    QuestionAnsweringSquadEvaluator,
+)
+from flow_portal.evaluation.evaluators.schemas import TraceEvaluationResult
+from flow_portal.tracing import TracingProcessor
+from flow_portal.tracing.trace import AgentTrace
+
+
+def evaluate(
+    evaluation_case: EvaluationCase,
+    trace: AgentTrace,
+    agent_framework: AgentFramework,
+) -> TraceEvaluationResult:
+    checkpoint_evaluator = CheckpointEvaluator(model=evaluation_case.llm_judge)
+    hypothesis_evaluator = HypothesisEvaluator(model=evaluation_case.llm_judge)
+    qa_evaluator = QuestionAnsweringSquadEvaluator()
+    processor = TracingProcessor.create(agent_framework)
+    if not processor:
+        msg = f"Processor for {agent_framework} not available."
+        raise ValueError(msg)
+    hypothesis_answer = processor._extract_hypothesis_answer(trace=trace)
+    checkpoint_results = checkpoint_evaluator.evaluate(
+        trace=trace,
+        checkpoints=evaluation_case.checkpoints,
+        processor=processor,
+    )
+    hypothesis_answer_results = hypothesis_evaluator.evaluate(
+        hypothesis_final_output=hypothesis_answer,
+        ground_truth_answer_dict=evaluation_case.ground_truth,
+        ground_truth_checkpoints=evaluation_case.final_output_criteria,
+    )
+
+    if evaluation_case.ground_truth:
+        direct_results = qa_evaluator.evaluate(
+            hypothesis_answer=hypothesis_answer,
+            ground_truth_answer=evaluation_case.ground_truth,
+        )
+    else:
+        direct_results = []
+
+    return TraceEvaluationResult(
+        trace=trace,
+        hypothesis_answer=hypothesis_answer,
+        checkpoint_results=checkpoint_results,
+        hypothesis_answer_results=hypothesis_answer_results,
+        direct_results=direct_results,
+    )

tests/docs/test_all.py
@@ -4,22 +4,22 @@ from unittest.mock import AsyncMock, MagicMock, patch
 import pytest
 from mktestdocs import check_md_file
 
+from flow_portal.evaluation.evaluators.schemas import TraceEvaluationResult
+
 
 # Note the use of `str`, makes for pretty output
 @pytest.mark.parametrize("fpath", pathlib.Path("docs").glob("**/*.md"), ids=str)
 def test_files_all(fpath: pathlib.Path) -> None:
     mock_agent = MagicMock()
     mock_create = MagicMock(return_value=mock_agent)
+    mock_eval = MagicMock()
+    mock_eval.return_value = MagicMock(spec=TraceEvaluationResult)
 
     mock_create_async = AsyncMock()
     with (
         patch("builtins.open", new_callable=MagicMock),
-        patch(
-            "flow_portal.evaluation.evaluation_runner.save_evaluation_results",
-            return_value=None,
-        ),
-        patch("flow_portal.evaluation.evaluation_runner.EvaluationRunner.run"),
         patch("flow_portal.AnyAgent.create", mock_create),
+        patch("flow_portal.evaluation.evaluate", mock_eval),
         patch("flow_portal.AnyAgent.create_async", mock_create_async),
     ):
         check_md_file(fpath=fpath, memory=True)  # type: ignore[no-untyped-call]

tests/unit/evaluation/test_evaluate.py
@@ -1,0 +1,132 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from flow_portal.config import AgentFramework
+from flow_portal.evaluation import EvaluationCase, evaluate
+from flow_portal.evaluation.evaluators.schemas import (
+    EvaluationResult,
+    TraceEvaluationResult,
+)
+from flow_portal.tracing.trace import AgentTrace
+
+
+def test_evaluate_runs_all_evaluators(
+    evaluation_case: EvaluationCase, agent_trace: AgentTrace
+) -> None:
+    """This unit test is designed to ensure that the EvaluationRunner properly iterates over all the traces and eval cases"""
+    #### Set up the mocks for the evaluators so that we don't actually call LLMs.
+    mock_checkpoint_evaluator = MagicMock()
+    mock_hypothesis_evaluator = MagicMock()
+    mock_qa_evaluator = MagicMock()
+
+    ### Every evaluator will return the same result
+    eval_result = [
+        EvaluationResult(
+            criteria="test criteria", passed=True, reason="test passed", points=1
+        )
+    ]
+    for evaluator in [
+        mock_checkpoint_evaluator,
+        mock_hypothesis_evaluator,
+        mock_qa_evaluator,
+    ]:
+        evaluator.evaluate.return_value = eval_result
+
+    with (
+        patch(
+            "flow_portal.evaluation.evaluate.CheckpointEvaluator"
+        ) as mock_checkpoint_eval_cls,
+        patch(
+            "flow_portal.evaluation.evaluate.HypothesisEvaluator"
+        ) as mock_hypothesis_eval_cls,
+        patch(
+            "flow_portal.evaluation.evaluate.QuestionAnsweringSquadEvaluator"
+        ) as mock_qa_eval_cls,
+        patch(
+            "flow_portal.evaluation.evaluate.TracingProcessor.create"
+        ) as mock_processor_create,
+    ):
+        # Evaluators are created for each evaluation case (but not for each trace)
+        mock_checkpoint_eval_cls.side_effect = [
+            mock_checkpoint_evaluator,
+        ]
+        mock_hypothesis_eval_cls.side_effect = [mock_hypothesis_evaluator]
+        mock_qa_eval_cls.side_effect = [mock_qa_evaluator]
+
+        # Mock processor is used to extract the hypothesis answer
+        mock_processor = MagicMock()
+        mock_processor._extract_hypothesis_answer.return_value = (
+            "Mock hypothesis answer"
+        )
+        mock_processor_create.return_value = mock_processor
+
+        evaluate(
+            evaluation_case=evaluation_case,
+            trace=agent_trace,
+            agent_framework=AgentFramework.OPENAI,
+        )
+
+        assert mock_checkpoint_eval_cls.call_count == 1
+        assert mock_hypothesis_eval_cls.call_count == 1
+        assert mock_qa_eval_cls.call_count == 1
+
+        assert mock_checkpoint_evaluator.evaluate.call_count == 1
+        assert mock_hypothesis_evaluator.evaluate.call_count == 1
+        assert mock_qa_evaluator.evaluate.call_count == 1
+
+
+def test_trace_evaluation_result_score_calculation(agent_trace: AgentTrace) -> None:
+    """Test that the score property of TraceEvaluationResult correctly calculates the ratio of passed points to total points."""
+
+    # Create evaluation results with different point values and pass status
+    checkpoint_results = [
+        EvaluationResult(
+            criteria="Criterion 1", passed=True, reason="Passed", points=2
+        ),
+        EvaluationResult(
+            criteria="Criterion 2", passed=False, reason="Failed", points=3
+        ),
+    ]
+
+    hypothesis_results = [
+        EvaluationResult(
+            criteria="Hypothesis 1", passed=True, reason="Passed", points=5
+        ),
+        EvaluationResult(
+            criteria="Hypothesis 2", passed=False, reason="Failed", points=1
+        ),
+    ]
+
+    direct_results = [
+        EvaluationResult(criteria="Direct 1", passed=True, reason="Passed", points=3),
+        EvaluationResult(criteria="Direct 2", passed=True, reason="Passed", points=4),
+        EvaluationResult(criteria="Direct 3", passed=False, reason="Failed", points=2),
+    ]
+
+    # Create a TraceEvaluationResult instance
+    evaluation_result = TraceEvaluationResult(
+        trace=agent_trace,
+        hypothesis_answer="Test hypothesis",
+        checkpoint_results=checkpoint_results,
+        hypothesis_answer_results=hypothesis_results,
+        direct_results=direct_results,
+    )
+
+    expected_score = 14 / 20
+
+    # Check that the score property returns the correct value
+    assert evaluation_result.score == expected_score, (
+        f"Expected score {expected_score}, got {evaluation_result.score}"
+    )
+
+    # Test case with no points (should raise ValueError)
+    zero_point_result = TraceEvaluationResult(
+        trace=agent_trace,
+        hypothesis_answer="Test hypothesis",
+        checkpoint_results=[],
+        hypothesis_answer_results=[],
+        direct_results=[],
+    )
+    with pytest.raises(ValueError, match="Total points is 0, cannot calculate score."):
+        zero_point_result.score  # noqa: B018

mozilla-ai/any-agent

src/any_agent/evaluation/cli.py
@@ -1,42 +1,42 @@
 import json
+import os
 
 from fire import Fire
 
 from any_agent.config import AgentFramework
-from any_agent.evaluation import EvaluationRunner
-from any_agent.evaluation.evaluation_case import EvaluationCase
+from any_agent.evaluation import EvaluationCase, evaluate
 from any_agent.logging import logger
 from any_agent.tracing.trace import AgentSpan, AgentTrace
 
 
 def do_eval(
-    evaluation_case_paths: list[str],
-    trace_paths: list[str],
+    evaluation_case_path: str,
+    trace_path: str,
     agent_framework: AgentFramework,
     output_path: str = "output/results.json",
 ) -> None:
     logger.info("Starting evaluation...")
-    runner = EvaluationRunner(output_path=output_path)
+    logger.info("Loading test case from %s", evaluation_case_path)
+    evaluation_case = EvaluationCase.from_yaml(evaluation_case_path)
+    logger.info("Loading tracing from %s", trace_path)
+    with open(trace_path, encoding="utf-8") as f:
+        spans = json.loads(f.read())
+    spans = [AgentSpan.model_validate_json(span) for span in spans]
+    trace = AgentTrace(spans=spans)
+    # dump the trace to a file
+    with open("tmp.tmp", "w", encoding="utf-8") as f:
+        f.write(trace.model_dump_json(indent=2))
+    result = evaluate(
+        evaluation_case=evaluation_case,
+        trace=trace,
+        agent_framework=agent_framework,
+    )
+    logger.info(f"Final score: {result.score}")
 
-    for evaluation_case_path in evaluation_case_paths:
-        logger.info("Loading test case from %s", evaluation_case_path)
-        evaluation_case = EvaluationCase.from_yaml(evaluation_case_path)
-        runner.add_evaluation_case(evaluation_case)
-
-    for trace_path in trace_paths:
-        logger.info("Loading tracing from %s", trace_path)
-        with open(trace_path, encoding="utf-8") as f:
-            spans = json.loads(f.read())
-        spans = [AgentSpan.model_validate_json(span) for span in spans]
-        trace = AgentTrace(spans=spans)
-        # dump the trace to a file
-        with open("tmp.tmp", "w", encoding="utf-8") as f:
-            f.write(trace.model_dump_json(indent=2))
-        runner.add_trace(trace, agent_framework)
-
-    logger.info("Running evaluation...")
-    runner.run()
-    logger.info("Evaluation completed.")
+    logger.info("Writing results to %s", output_path)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(result.model_dump_json(indent=2))
 
 
 def main() -> None:

src/any_agent/evaluation/evaluate.py
@@ -1,0 +1,51 @@
+from any_agent.config import AgentFramework
+from any_agent.evaluation.evaluation_case import EvaluationCase
+from any_agent.evaluation.evaluators import (
+    CheckpointEvaluator,
+    HypothesisEvaluator,
+    QuestionAnsweringSquadEvaluator,
+)
+from any_agent.evaluation.evaluators.schemas import TraceEvaluationResult
+from any_agent.tracing import TracingProcessor
+from any_agent.tracing.trace import AgentTrace
+
+
+def evaluate(
+    evaluation_case: EvaluationCase,
+    trace: AgentTrace,
+    agent_framework: AgentFramework,
+) -> TraceEvaluationResult:
+    checkpoint_evaluator = CheckpointEvaluator(model=evaluation_case.llm_judge)
+    hypothesis_evaluator = HypothesisEvaluator(model=evaluation_case.llm_judge)
+    qa_evaluator = QuestionAnsweringSquadEvaluator()
+    processor = TracingProcessor.create(agent_framework)
+    if not processor:
+        msg = f"Processor for {agent_framework} not available."
+        raise ValueError(msg)
+    hypothesis_answer = processor._extract_hypothesis_answer(trace=trace)
+    checkpoint_results = checkpoint_evaluator.evaluate(
+        trace=trace,
+        checkpoints=evaluation_case.checkpoints,
+        processor=processor,
+    )
+    hypothesis_answer_results = hypothesis_evaluator.evaluate(
+        hypothesis_final_output=hypothesis_answer,
+        ground_truth_answer_dict=evaluation_case.ground_truth,
+        ground_truth_checkpoints=evaluation_case.final_output_criteria,
+    )
+
+    if evaluation_case.ground_truth:
+        direct_results = qa_evaluator.evaluate(
+            hypothesis_answer=hypothesis_answer,
+            ground_truth_answer=evaluation_case.ground_truth,
+        )
+    else:
+        direct_results = []
+
+    return TraceEvaluationResult(
+        trace=trace,
+        hypothesis_answer=hypothesis_answer,
+        checkpoint_results=checkpoint_results,
+        hypothesis_answer_results=hypothesis_answer_results,
+        direct_results=direct_results,
+    )

tests/docs/test_all.py
@@ -4,22 +4,22 @@ from unittest.mock import AsyncMock, MagicMock, patch
 import pytest
 from mktestdocs import check_md_file
 
+from any_agent.evaluation.evaluators.schemas import TraceEvaluationResult
+
 
 # Note the use of `str`, makes for pretty output
 @pytest.mark.parametrize("fpath", pathlib.Path("docs").glob("**/*.md"), ids=str)
 def test_files_all(fpath: pathlib.Path) -> None:
     mock_agent = MagicMock()
     mock_create = MagicMock(return_value=mock_agent)
+    mock_eval = MagicMock()
+    mock_eval.return_value = MagicMock(spec=TraceEvaluationResult)
 
     mock_create_async = AsyncMock()
     with (
         patch("builtins.open", new_callable=MagicMock),
-        patch(
-            "any_agent.evaluation.evaluation_runner.save_evaluation_results",
-            return_value=None,
-        ),
-        patch("any_agent.evaluation.evaluation_runner.EvaluationRunner.run"),
         patch("any_agent.AnyAgent.create", mock_create),
+        patch("any_agent.evaluation.evaluate", mock_eval),
         patch("any_agent.AnyAgent.create_async", mock_create_async),
     ):
         check_md_file(fpath=fpath, memory=True)  # type: ignore[no-untyped-call]

tests/unit/evaluation/test_evaluate.py
@@ -1,0 +1,132 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from any_agent.config import AgentFramework
+from any_agent.evaluation import EvaluationCase, evaluate
+from any_agent.evaluation.evaluators.schemas import (
+    EvaluationResult,
+    TraceEvaluationResult,
+)
+from any_agent.tracing.trace import AgentTrace
+
+
+def test_evaluate_runs_all_evaluators(
+    evaluation_case: EvaluationCase, agent_trace: AgentTrace
+) -> None:
+    """This unit test is designed to ensure that the EvaluationRunner properly iterates over all the traces and eval cases"""
+    #### Set up the mocks for the evaluators so that we don't actually call LLMs.
+    mock_checkpoint_evaluator = MagicMock()
+    mock_hypothesis_evaluator = MagicMock()
+    mock_qa_evaluator = MagicMock()
+
+    ### Every evaluator will return the same result
+    eval_result = [
+        EvaluationResult(
+            criteria="test criteria", passed=True, reason="test passed", points=1
+        )
+    ]
+    for evaluator in [
+        mock_checkpoint_evaluator,
+        mock_hypothesis_evaluator,
+        mock_qa_evaluator,
+    ]:
+        evaluator.evaluate.return_value = eval_result
+
+    with (
+        patch(
+            "any_agent.evaluation.evaluate.CheckpointEvaluator"
+        ) as mock_checkpoint_eval_cls,
+        patch(
+            "any_agent.evaluation.evaluate.HypothesisEvaluator"
+        ) as mock_hypothesis_eval_cls,
+        patch(
+            "any_agent.evaluation.evaluate.QuestionAnsweringSquadEvaluator"
+        ) as mock_qa_eval_cls,
+        patch(
+            "any_agent.evaluation.evaluate.TracingProcessor.create"
+        ) as mock_processor_create,
+    ):
+        # Evaluators are created for each evaluation case (but not for each trace)
+        mock_checkpoint_eval_cls.side_effect = [
+            mock_checkpoint_evaluator,
+        ]
+        mock_hypothesis_eval_cls.side_effect = [mock_hypothesis_evaluator]
+        mock_qa_eval_cls.side_effect = [mock_qa_evaluator]
+
+        # Mock processor is used to extract the hypothesis answer
+        mock_processor = MagicMock()
+        mock_processor._extract_hypothesis_answer.return_value = (
+            "Mock hypothesis answer"
+        )
+        mock_processor_create.return_value = mock_processor
+
+        evaluate(
+            evaluation_case=evaluation_case,
+            trace=agent_trace,
+            agent_framework=AgentFramework.OPENAI,
+        )
+
+        assert mock_checkpoint_eval_cls.call_count == 1
+        assert mock_hypothesis_eval_cls.call_count == 1
+        assert mock_qa_eval_cls.call_count == 1
+
+        assert mock_checkpoint_evaluator.evaluate.call_count == 1
+        assert mock_hypothesis_evaluator.evaluate.call_count == 1
+        assert mock_qa_evaluator.evaluate.call_count == 1
+
+
+def test_trace_evaluation_result_score_calculation(agent_trace: AgentTrace) -> None:
+    """Test that the score property of TraceEvaluationResult correctly calculates the ratio of passed points to total points."""
+
+    # Create evaluation results with different point values and pass status
+    checkpoint_results = [
+        EvaluationResult(
+            criteria="Criterion 1", passed=True, reason="Passed", points=2
+        ),
+        EvaluationResult(
+            criteria="Criterion 2", passed=False, reason="Failed", points=3
+        ),
+    ]
+
+    hypothesis_results = [
+        EvaluationResult(
+            criteria="Hypothesis 1", passed=True, reason="Passed", points=5
+        ),
+        EvaluationResult(
+            criteria="Hypothesis 2", passed=False, reason="Failed", points=1
+        ),
+    ]
+
+    direct_results = [
+        EvaluationResult(criteria="Direct 1", passed=True, reason="Passed", points=3),
+        EvaluationResult(criteria="Direct 2", passed=True, reason="Passed", points=4),
+        EvaluationResult(criteria="Direct 3", passed=False, reason="Failed", points=2),
+    ]
+
+    # Create a TraceEvaluationResult instance
+    evaluation_result = TraceEvaluationResult(
+        trace=agent_trace,
+        hypothesis_answer="Test hypothesis",
+        checkpoint_results=checkpoint_results,
+        hypothesis_answer_results=hypothesis_results,
+        direct_results=direct_results,
+    )
+
+    expected_score = 14 / 20
+
+    # Check that the score property returns the correct value
+    assert evaluation_result.score == expected_score, (
+        f"Expected score {expected_score}, got {evaluation_result.score}"
+    )
+
+    # Test case with no points (should raise ValueError)
+    zero_point_result = TraceEvaluationResult(
+        trace=agent_trace,
+        hypothesis_answer="Test hypothesis",
+        checkpoint_results=[],
+        hypothesis_answer_results=[],
+        direct_results=[],
+    )
+    with pytest.raises(ValueError, match="Total points is 0, cannot calculate score."):
+        zero_point_result.score  # noqa: B018