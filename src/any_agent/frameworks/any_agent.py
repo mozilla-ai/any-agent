flowportal-ai/belt

docs: Revisit API Reference. (#212)
- Split into subpages.
- Replace config info with links to the API Reference.
- Remove `Params` suffix from MCP config.
- Add `_` prefix to private methods.

src/any_agent/frameworks/any_agent.py
@@ -16,12 +16,12 @@ from flow_portal.tracing.exporter import (
     Instrumenter,
     get_instrumenter_by_framework,
 )
-from flow_portal.tracing.trace import is_tracing_supported
+from flow_portal.tracing.trace import _is_tracing_supported
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
-    from flow_portal.tools.mcp.mcp_server import MCPServerBase
+    from flow_portal.tools.mcp.mcp_server import _MCPServerBase
     from flow_portal.tracing.trace import AgentTrace
 
 
@@ -40,7 +40,7 @@ class AnyAgent(ABC):
         self.config = config
         self.managed_agents = managed_agents
 
-        self._mcp_servers: list[MCPServerBase[Any]] = []
+        self._mcp_servers: list[_MCPServerBase[Any]] = []
 
         # Tracing is enabled by default
         self._tracing_config: TracingConfig = tracing or TracingConfig()
@@ -98,6 +98,7 @@ class AnyAgent(ABC):
         managed_agents: list[AgentConfig] | None = None,
         tracing: TracingConfig | None = None,
     ) -> AnyAgent:
+        """Create an agent using the given framework and config."""
         return asyncio.get_event_loop().run_until_complete(
             cls.create_async(
                 agent_framework=agent_framework,
@@ -115,14 +116,15 @@ class AnyAgent(ABC):
         managed_agents: list[AgentConfig] | None = None,
         tracing: TracingConfig | None = None,
     ) -> AnyAgent:
+        """Create an agent using the given framework and config."""
         agent_cls = cls._get_agent_type_by_framework(agent_framework)
         agent = agent_cls(agent_config, managed_agents=managed_agents, tracing=tracing)
-        await agent.load_agent()
+        await agent._load_agent()
         return agent
 
     async def _load_tools(
         self, tools: Sequence[Tool]
-    ) -> tuple[list[Any], list[MCPServerBase[Any]]]:
+    ) -> tuple[list[Any], list[_MCPServerBase[Any]]]:
         tools, mcp_servers = await _wrap_tools(tools, self.framework)
         # Add to agent so that it doesn't get garbage collected
         self._mcp_servers.extend(mcp_servers)
@@ -140,7 +142,7 @@ class AnyAgent(ABC):
 
         # Agno not yet supported https://github.com/Arize-ai/openinference/issues/1302
         # Google ADK not yet supported https://github.com/Arize-ai/openinference/issues/1506
-        if not is_tracing_supported(self.framework):
+        if not _is_tracing_supported(self.framework):
             logger.warning(
                 "Tracing is not yet supported for AGNO and GOOGLE frameworks. "
             )
@@ -161,7 +163,7 @@ class AnyAgent(ABC):
         )
 
     @abstractmethod
-    async def load_agent(self) -> None:
+    async def _load_agent(self) -> None:
         """Load the agent instance."""
 
     @abstractmethod

tests/unit/tools/mcp/test_unit_agno_mcp.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 import pytest
 from agno.tools.mcp import MCPTools as AgnoMCPTools
 
-from flow_portal.config import AgentFramework, MCPSseParams, Tool
+from flow_portal.config import AgentFramework, MCPSse, Tool
 from flow_portal.tools import _get_mcp_server
 
 
@@ -20,7 +20,7 @@ def agno_mcp_tools() -> Generator[AgnoMCPTools]:
     "enter_context_with_transport_and_session",
 )
 async def test_agno_mcp_sse_integration(
-    mcp_sse_params_with_tools: MCPSseParams,
+    mcp_sse_params_with_tools: MCPSse,
     session: Any,
     tools: Sequence[Tool],
     agno_mcp_tools: AgnoMCPTools,

tests/unit/tools/mcp/test_unit_google_mcp.py
@@ -7,7 +7,7 @@ from google.adk.tools.mcp_tool.mcp_toolset import (  # type: ignore[attr-defined
     SseServerParams as GoogleSseServerParameters,
 )
 
-from flow_portal.config import AgentFramework, MCPSseParams, Tool
+from flow_portal.config import AgentFramework, MCPSse, Tool
 from flow_portal.tools import _get_mcp_server
 
 
@@ -34,7 +34,7 @@ def google_toolset(tools: Sequence[Tool]) -> Generator[GoogleMCPToolset]:
 async def test_google_mcp_sse_integration(
     google_toolset: GoogleMCPToolset,
     google_sse_params: GoogleSseServerParameters,
-    mcp_sse_params_no_tools: MCPSseParams,
+    mcp_sse_params_no_tools: MCPSse,
 ) -> None:
     mcp_server = _get_mcp_server(mcp_sse_params_no_tools, AgentFramework.GOOGLE)
     await mcp_server._setup_tools()

tests/unit/tools/mcp/test_unit_langchain_mcp_sse.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
-from flow_portal.config import AgentFramework, MCPSseParams, Tool
+from flow_portal.config import AgentFramework, MCPSse, Tool
 from flow_portal.tools import _get_mcp_server
 
 
@@ -24,7 +24,7 @@ def load_mcp_tools(
     "enter_context_with_transport_and_session", "_path_client_session"
 )
 async def test_langchain_mcp_sse_integration(
-    mcp_sse_params_no_tools: MCPSseParams,
+    mcp_sse_params_no_tools: MCPSse,
     session: Any,
     load_mcp_tools: Any,
 ) -> None:

tests/unit/tools/mcp/test_unit_llama_index_mcp.py
@@ -5,7 +5,7 @@ import pytest
 from llama_index.tools.mcp import BasicMCPClient as LlamaIndexMCPClient
 from llama_index.tools.mcp import McpToolSpec as LlamaIndexMcpToolSpec
 
-from flow_portal.config import AgentFramework, MCPSseParams, Tool
+from flow_portal.config import AgentFramework, MCPSse, Tool
 from flow_portal.tools import _get_mcp_server
 
 
@@ -35,7 +35,7 @@ def llama_index_mcp_tool_spec(
     "llama_index_mcp_tool_spec",
 )
 async def test_llamaindex_mcp_sse_integration(
-    mcp_sse_params_with_tools: MCPSseParams,
+    mcp_sse_params_with_tools: MCPSse,
     llama_index_mcp_client: LlamaIndexMCPClient,
 ) -> None:
     server = _get_mcp_server(mcp_sse_params_with_tools, AgentFramework.LLAMA_INDEX)

tests/unit/tools/mcp/test_unit_smolagents_mcp.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 import pytest
 from smolagents.mcp_client import MCPClient
 
-from flow_portal.config import AgentFramework, MCPSseParams, Tool
+from flow_portal.config import AgentFramework, MCPSse, Tool
 from flow_portal.tools import _get_mcp_server
 
 
@@ -22,7 +22,7 @@ def smolagents_mcp_server(
 
 @pytest.mark.asyncio
 async def test_smolagents_mcp_sse_integration(
-    mcp_sse_params_no_tools: MCPSseParams,
+    mcp_sse_params_no_tools: MCPSse,
     smolagents_mcp_server: MCPClient,
 ) -> None:
     server = _get_mcp_server(mcp_sse_params_no_tools, AgentFramework.SMOLAGENTS)

tests/unit/tracing/test_unit_processors.py
@@ -3,13 +3,13 @@ from opentelemetry.sdk.trace import ReadableSpan
 
 from flow_portal import AgentFramework
 from flow_portal.tracing import TracingProcessor
-from flow_portal.tracing.trace import AgentSpan, is_tracing_supported
+from flow_portal.tracing.trace import AgentSpan, _is_tracing_supported
 
 
 def test_extract_interaction(
     agent_framework: AgentFramework, llm_span: ReadableSpan
 ) -> None:
-    if not is_tracing_supported(agent_framework):
+    if not _is_tracing_supported(agent_framework):
         pytest.skip()
     processor = TracingProcessor.create(AgentFramework(agent_framework))
     # to make mypy happy

mozilla-ai/any-agent

src/any_agent/frameworks/any_agent.py
@@ -16,12 +16,12 @@ from any_agent.tracing.exporter import (
     Instrumenter,
     get_instrumenter_by_framework,
 )
-from any_agent.tracing.trace import is_tracing_supported
+from any_agent.tracing.trace import _is_tracing_supported
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
-    from any_agent.mcp.mcp_server import MCPServerBase
+    from any_agent.tools.mcp.mcp_server import _MCPServerBase
     from any_agent.tracing.trace import AgentTrace
 
 
@@ -40,7 +40,7 @@ class AnyAgent(ABC):
         self.config = config
         self.managed_agents = managed_agents
 
-        self._mcp_servers: list[MCPServerBase[Any]] = []
+        self._mcp_servers: list[_MCPServerBase[Any]] = []
 
         # Tracing is enabled by default
         self._tracing_config: TracingConfig = tracing or TracingConfig()
@@ -98,6 +98,7 @@ class AnyAgent(ABC):
         managed_agents: list[AgentConfig] | None = None,
         tracing: TracingConfig | None = None,
     ) -> AnyAgent:
+        """Create an agent using the given framework and config."""
         return asyncio.get_event_loop().run_until_complete(
             cls.create_async(
                 agent_framework=agent_framework,
@@ -115,15 +116,16 @@ class AnyAgent(ABC):
         managed_agents: list[AgentConfig] | None = None,
         tracing: TracingConfig | None = None,
     ) -> AnyAgent:
+        """Create an agent using the given framework and config."""
         agent_cls = cls._get_agent_type_by_framework(agent_framework)
         agent = agent_cls(agent_config, managed_agents=managed_agents, tracing=tracing)
-        await agent.load_agent()
+        await agent._load_agent()
         return agent
 
     async def _load_tools(
         self, tools: Sequence[Tool]
-    ) -> tuple[list[AnyTool], list[MCPServerBase[Any]]]:
-        tools_, mcp_servers = await wrap_tools(tools, self.framework)
+    ) -> tuple[list[Any], list[_MCPServerBase[Any]]]:
+        tools, mcp_servers = await _wrap_tools(tools, self.framework)
         # Add to agent so that it doesn't get garbage collected
         self._mcp_servers.extend(mcp_servers)
         for mcp_server in mcp_servers:
@@ -140,7 +142,7 @@ class AnyAgent(ABC):
 
         # Agno not yet supported https://github.com/Arize-ai/openinference/issues/1302
         # Google ADK not yet supported https://github.com/Arize-ai/openinference/issues/1506
-        if not is_tracing_supported(self.framework):
+        if not _is_tracing_supported(self.framework):
             logger.warning(
                 "Tracing is not yet supported for AGNO and GOOGLE frameworks. "
             )
@@ -161,7 +163,7 @@ class AnyAgent(ABC):
         )
 
     @abstractmethod
-    async def load_agent(self) -> None:
+    async def _load_agent(self) -> None:
         """Load the agent instance."""
 
     @abstractmethod

tests/unit/mcp/test_unit_agno_mcp.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 import pytest
 from agno.tools.mcp import MCPTools as AgnoMCPTools
 
-from any_agent.config import AgentFramework, MCPSseParams, Tool
+from any_agent.config import AgentFramework, MCPSse, Tool
 from any_agent.mcp import _get_mcp_server
 
 
@@ -20,7 +20,7 @@ def agno_mcp_tools() -> Generator[AgnoMCPTools]:
     "enter_context_with_transport_and_session",
 )
 async def test_agno_mcp_sse_integration(
-    mcp_sse_params_with_tools: MCPSseParams,
+    mcp_sse_params_with_tools: MCPSse,
     session: Any,
     tools: Sequence[Tool],
     agno_mcp_tools: AgnoMCPTools,

tests/unit/mcp/test_unit_google_mcp.py
@@ -7,7 +7,7 @@ from google.adk.tools.mcp_tool.mcp_toolset import (  # type: ignore[attr-defined
     SseServerParams as GoogleSseServerParameters,
 )
 
-from any_agent.config import AgentFramework, MCPSseParams, Tool
+from any_agent.config import AgentFramework, MCPSse, Tool
 from any_agent.mcp import _get_mcp_server
 
 
@@ -34,7 +34,7 @@ def google_toolset(tools: Sequence[Tool]) -> Generator[GoogleMCPToolset]:
 async def test_google_mcp_sse_integration(
     google_toolset: GoogleMCPToolset,
     google_sse_params: GoogleSseServerParameters,
-    mcp_sse_params_no_tools: MCPSseParams,
+    mcp_sse_params_no_tools: MCPSse,
 ) -> None:
     mcp_server = _get_mcp_server(mcp_sse_params_no_tools, AgentFramework.GOOGLE)
     await mcp_server._setup_tools()

tests/unit/mcp/test_unit_langchain_mcp_sse.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
-from any_agent.config import AgentFramework, MCPSseParams, Tool
+from any_agent.config import AgentFramework, MCPSse, Tool
 from any_agent.mcp import _get_mcp_server
 
 
@@ -22,7 +22,7 @@ def load_mcp_tools(
     "enter_context_with_transport_and_session", "_path_client_session"
 )
 async def test_langchain_mcp_sse_integration(
-    mcp_sse_params_no_tools: MCPSseParams,
+    mcp_sse_params_no_tools: MCPSse,
     session: Any,
     load_mcp_tools: Any,
 ) -> None:

tests/unit/mcp/test_unit_llama_index_mcp.py
@@ -5,7 +5,7 @@ import pytest
 from llama_index.tools.mcp import BasicMCPClient as LlamaIndexMCPClient
 from llama_index.tools.mcp import McpToolSpec as LlamaIndexMcpToolSpec
 
-from any_agent.config import AgentFramework, MCPSseParams, Tool
+from any_agent.config import AgentFramework, MCPSse, Tool
 from any_agent.mcp import _get_mcp_server
 
 
@@ -35,7 +35,7 @@ def llama_index_mcp_tool_spec(
     "llama_index_mcp_tool_spec",
 )
 async def test_llamaindex_mcp_sse_integration(
-    mcp_sse_params_with_tools: MCPSseParams,
+    mcp_sse_params_with_tools: MCPSse,
     llama_index_mcp_client: LlamaIndexMCPClient,
 ) -> None:
     server = _get_mcp_server(mcp_sse_params_with_tools, AgentFramework.LLAMA_INDEX)

tests/unit/mcp/test_unit_smolagents_mcp.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 import pytest
 from smolagents.mcp_client import MCPClient
 
-from any_agent.config import AgentFramework, MCPSseParams, Tool
+from any_agent.config import AgentFramework, MCPSse, Tool
 from any_agent.mcp import _get_mcp_server
 
 
@@ -20,7 +20,7 @@ def smolagents_mcp_server(
 
 @pytest.mark.asyncio
 async def test_smolagents_mcp_sse_integration(
-    mcp_sse_params_no_tools: MCPSseParams,
+    mcp_sse_params_no_tools: MCPSse,
     smolagents_mcp_server: MCPClient,
 ) -> None:
     server = _get_mcp_server(mcp_sse_params_no_tools, AgentFramework.SMOLAGENTS)

tests/unit/tracing/test_unit_processors.py
@@ -3,13 +3,13 @@ from opentelemetry.sdk.trace import ReadableSpan
 
 from any_agent import AgentFramework
 from any_agent.tracing import TracingProcessor
-from any_agent.tracing.trace import AgentSpan, is_tracing_supported
+from any_agent.tracing.trace import AgentSpan, _is_tracing_supported
 
 
 def test_extract_interaction(
     agent_framework: AgentFramework, llm_span: ReadableSpan
 ) -> None:
-    if not is_tracing_supported(agent_framework):
+    if not _is_tracing_supported(agent_framework):
         pytest.skip()
     processor = TracingProcessor.create(AgentFramework(agent_framework))
     # to make mypy happy