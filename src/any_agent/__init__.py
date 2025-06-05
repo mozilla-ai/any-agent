268c6886cf4895b81fdcf4dd21523376f197f25b
Javier Torres <javier@mozilla.ai>
2025-06-05 13:31:08 +0200

    Initial implementation

--- src/any_agent/__init__.py
+++ src/any_agent/__init__.py
@@ -1,10 +1,11 @@
 from .config import AgentConfig, AgentFramework, TracingConfig
-from .frameworks.any_agent import AnyAgent
+from .frameworks.any_agent import AgentRunException, AnyAgent
 from .tracing.agent_trace import AgentTrace
 
 __all__ = [
     "AgentConfig",
     "AgentFramework",
+    "AgentRunException",
     "AgentTrace",
     "AnyAgent",
     "TracingConfig",
