864129aede3213919f86d0be2a7be3628fff1f3e
Hareesh Bahuleyan <hareeshbahuleyan@gmail.com>
2025-06-17 18:05:47 +0200

    Using importlib.metadata

--- src/any_agent/__init__.py
+++ src/any_agent/__init__.py
@@ -1,4 +1,9 @@
-from ._version import version as __version__
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("any-agent")
+except PackageNotFoundError:
+    __version__ = "unknown"
 from .config import AgentConfig, AgentFramework
 from .frameworks.any_agent import AgentRunError, AnyAgent
 from .tracing.agent_trace import AgentTrace
