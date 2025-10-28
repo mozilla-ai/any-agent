346d0ed465c1e74d66c82a470f6862b41012a1d0
rta_phuongnguyen <phuongnguyen@rtanalytics.vn>
2025-10-28 16:47:58 +0700

    test tool

--- src/any_agent/tools/wrappers.py
+++ src/any_agent/tools/wrappers.py
@@ -56,9 +56,8 @@ def _wrap_tool_langchain(tool: "Tool | LangchainTool") -> "LangchainTool":
     print("tool",tool)
     # List all attributes
     for attr in dir(tool):
-        if not attr.startswith('__') and not attr.endswith('__'):
-            # skip dunder names if you want
-            print(f"{attr} = {getattr(tool, attr)}")
+        # skip dunder names if you want
+        print(f"{attr} = {getattr(tool, attr)}")
 
     return langchain_tool(tool)  # type: ignore[arg-type]
 
