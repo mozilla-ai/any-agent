fc1881319a406762f5f60baa610a3fe2ce8e493f
rta_phuongnguyen <phuongnguyen@rtanalytics.vn>
2025-10-28 15:42:36 +0700

    test

--- src/any_agent/frameworks/any_agent.py
+++ src/any_agent/frameworks/any_agent.py
@@ -219,7 +219,9 @@ class AnyAgent(ABC):
                         context, prompt, **kwargs
                     )
 
+                print("hereeee")
                 final_output = await self._run_async(prompt, **kwargs)
+                print("zzzzzzz")
 
         except Exception as e:
             print("hereeee",e)
